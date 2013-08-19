lkundrak/NetworkManager

commit d8e6065f634424b99a9e033144cfad3418c46159

core: switch nm-ip4-config's NMIP[46]Address to NMPlatformIP[46]Address

src/vpn-manager/nm-vpn-connection.c
@@ -602,8 +602,8 @@ static void
 print_vpn_config (NMVPNConnection *connection)
 {
 	NMVPNConnectionPrivate *priv = NM_VPN_CONNECTION_GET_PRIVATE (connection);
-	NMIP4Address *addr;
-	NMIP6Address *addr6;
+	const NMPlatformIP4Address *address4;
+	const NMPlatformIP6Address *address6;
 	char *dns_domain = NULL;
 	guint32 num, i;
 
@@ -620,12 +620,12 @@ print_vpn_config (NMVPNConnection *connection)
 	if (priv->ip4_config) {
 		nm_log_info (LOGD_VPN, "IPv4 configuration:");
 
-		addr = nm_ip4_config_get_address (priv->ip4_config, 0);
+		address4 = nm_ip4_config_get_address (priv->ip4_config, 0);
 
 		if (priv->ip4_internal_gw)
 			nm_log_info (LOGD_VPN, "  Internal Gateway: %s", ip_address_to_string (priv->ip4_internal_gw));
-		nm_log_info (LOGD_VPN, "  Internal Address: %s", ip_address_to_string (nm_ip4_address_get_address (addr)));
-		nm_log_info (LOGD_VPN, "  Internal Prefix: %d", nm_ip4_address_get_prefix (addr));
+		nm_log_info (LOGD_VPN, "  Internal Address: %s", ip_address_to_string (address4->address));
+		nm_log_info (LOGD_VPN, "  Internal Prefix: %d", address4->plen);
 		nm_log_info (LOGD_VPN, "  Internal Point-to-Point Address: %s",
 					 ip_address_to_string (nm_ip4_config_get_ptp_address (priv->ip4_config)));
 		nm_log_info (LOGD_VPN, "  Maximum Segment Size (MSS): %d", nm_ip4_config_get_mss (priv->ip4_config));
@@ -660,12 +660,12 @@ print_vpn_config (NMVPNConnection *connection)
 	if (priv->ip6_config) {
 		nm_log_info (LOGD_VPN, "IPv6 configuration:");
 
-		addr6 = nm_ip6_config_get_address (priv->ip6_config, 0);
+		address6 = nm_ip6_config_get_address (priv->ip6_config, 0);
 
 		if (priv->ip6_internal_gw)
 			nm_log_info (LOGD_VPN, "  Internal Gateway: %s", ip6_address_to_string (priv->ip6_internal_gw));
-		nm_log_info (LOGD_VPN, "  Internal Address: %s", ip6_address_to_string (nm_ip6_address_get_address (addr6)));
-		nm_log_info (LOGD_VPN, "  Internal Prefix: %d", nm_ip6_address_get_prefix (addr6));
+		nm_log_info (LOGD_VPN, "  Internal Address: %s", ip6_address_to_string (&address6->address));
+		nm_log_info (LOGD_VPN, "  Internal Prefix: %d", address6->plen);
 		nm_log_info (LOGD_VPN, "  Internal Point-to-Point Address: %s",
 					 ip6_address_to_string (nm_ip6_config_get_ptp_address (priv->ip6_config)));
 		nm_log_info (LOGD_VPN, "  Maximum Segment Size (MSS): %d", nm_ip6_config_get_mss (priv->ip6_config));
@@ -877,8 +877,7 @@ nm_vpn_connection_ip4_config_get (DBusGProxy *proxy,
 {
 	NMVPNConnection *connection = NM_VPN_CONNECTION (user_data);
 	NMVPNConnectionPrivate *priv = NM_VPN_CONNECTION_GET_PRIVATE (connection);
-	NMSettingIP4Config *s_ip4;
-	NMIP4Address *addr;
+	NMPlatformIP4Address address;
 	NMIP4Config *config;
 	GValue *val;
 	int i;
@@ -908,10 +907,10 @@ nm_vpn_connection_ip4_config_get (DBusGProxy *proxy,
 
 	config = nm_ip4_config_new ();
 
-	addr = nm_ip4_address_new ();
-	nm_ip4_address_set_prefix (addr, 24); /* default to class C */
+	memset (&address, 0, sizeof (address));
+	address.plen = 24;
 	if (priv->ip4_external_gw)
-		nm_ip4_address_set_gateway (addr, priv->ip4_external_gw);
+		nm_ip4_config_set_gateway (config, priv->ip4_external_gw);
 
 	/* Internal address of the VPN subnet's gateway */
 	val = (GValue *) g_hash_table_lookup (config_hash, NM_VPN_PLUGIN_IP4_CONFIG_INT_GATEWAY);
@@ -920,7 +919,7 @@ nm_vpn_connection_ip4_config_get (DBusGProxy *proxy,
 
 	val = (GValue *) g_hash_table_lookup (config_hash, NM_VPN_PLUGIN_IP4_CONFIG_ADDRESS);
 	if (val)
-		nm_ip4_address_set_address (addr, g_value_get_uint (val));
+		address.address = g_value_get_uint (val);
 
 	val = (GValue *) g_hash_table_lookup (config_hash, NM_VPN_PLUGIN_IP4_CONFIG_PTP);
 	if (val)
@@ -928,13 +927,12 @@ nm_vpn_connection_ip4_config_get (DBusGProxy *proxy,
 
 	val = (GValue *) g_hash_table_lookup (config_hash, NM_VPN_PLUGIN_IP4_CONFIG_PREFIX);
 	if (val)
-		nm_ip4_address_set_prefix (addr, g_value_get_uint (val));
+		address.plen = g_value_get_uint (val);
 
-	if (nm_ip4_address_get_address (addr) && nm_ip4_address_get_prefix (addr)) {
-		nm_ip4_config_take_address (config, addr);
+	if (address.address && address.plen) {
+		nm_ip4_config_add_address (config, &address);
 	} else {
 		nm_log_err (LOGD_VPN, "invalid IP4 config received!");
-		nm_ip4_address_unref (addr);
 		g_object_unref (config);
 		nm_vpn_connection_config_maybe_complete (connection, FALSE);
 		return;
@@ -1009,8 +1007,7 @@ nm_vpn_connection_ip4_config_get (DBusGProxy *proxy,
 		nm_ip4_config_set_never_default (config, g_value_get_boolean (val));
 
 	/* Merge in user overrides from the NMConnection's IPv4 setting */
-	s_ip4 = nm_connection_get_setting_ip4_config (priv->connection);
-	nm_ip4_config_merge_setting (config, s_ip4);
+	nm_ip4_config_merge_setting (config, nm_connection_get_setting_ip4_config (priv->connection));
 
 	priv->ip4_config = config;
 	nm_vpn_connection_config_maybe_complete (connection, TRUE);
@@ -1023,8 +1020,7 @@ nm_vpn_connection_ip6_config_get (DBusGProxy *proxy,
 {
 	NMVPNConnection *connection = NM_VPN_CONNECTION (user_data);
 	NMVPNConnectionPrivate *priv = NM_VPN_CONNECTION_GET_PRIVATE (connection);
-	NMSettingIP6Config *s_ip6;
-	NMIP6Address *addr;
+	NMPlatformIP6Address address;
 	NMIP6Config *config;
 	GValue *val;
 	int i;
@@ -1040,10 +1036,10 @@ nm_vpn_connection_ip6_config_get (DBusGProxy *proxy,
 
 	config = nm_ip6_config_new ();
 
-	addr = nm_ip6_address_new ();
-	nm_ip6_address_set_prefix (addr, 128); /* default to class C */
+	memset (&address, 0, sizeof (address));
+	address.plen = 128;
 	if (priv->ip6_external_gw)
-		nm_ip6_address_set_gateway (addr, priv->ip6_external_gw);
+		nm_ip6_config_set_gateway (config, priv->ip6_external_gw);
 
 	/* Internal address of the VPN subnet's gateway */
 	val = (GValue *) g_hash_table_lookup (config_hash, NM_VPN_PLUGIN_IP6_CONFIG_INT_GATEWAY);
@@ -1059,7 +1055,7 @@ nm_vpn_connection_ip6_config_get (DBusGProxy *proxy,
 		GByteArray *ba = g_value_get_boxed (val);
 
 		if (ba->len == sizeof (struct in6_addr))
-			nm_ip6_address_set_address (addr, (struct in6_addr *)ba->data);
+			address.address = *(struct in6_addr *) ba->data;
 	}
 
 	val = (GValue *) g_hash_table_lookup (config_hash, NM_VPN_PLUGIN_IP6_CONFIG_PTP);
@@ -1072,13 +1068,12 @@ nm_vpn_connection_ip6_config_get (DBusGProxy *proxy,
 
 	val = (GValue *) g_hash_table_lookup (config_hash, NM_VPN_PLUGIN_IP6_CONFIG_PREFIX);
 	if (val)
-		nm_ip6_address_set_prefix (addr, g_value_get_uint (val));
+		address.plen = g_value_get_uint (val);
 
-	if (nm_ip6_address_get_address (addr) && nm_ip6_address_get_prefix (addr)) {
-		nm_ip6_config_take_address (config, addr);
-	} else {
+	if (IN6_IS_ADDR_UNSPECIFIED (&address.address) && address.plen)
+		nm_ip6_config_add_address (config, &address);
+	else {
 		nm_log_err (LOGD_VPN, "invalid IP6 config received!");
-		nm_ip6_address_unref (addr);
 		g_object_unref (config);
 		nm_vpn_connection_config_maybe_complete (connection, FALSE);
 	}
@@ -1146,8 +1141,7 @@ nm_vpn_connection_ip6_config_get (DBusGProxy *proxy,
 		nm_ip6_config_set_never_default (config, g_value_get_boolean (val));
 
 	/* Merge in user overrides from the NMConnection's IPv6 setting */
-	s_ip6 = nm_connection_get_setting_ip6_config (priv->connection);
-	nm_ip6_config_merge_setting (config, s_ip6);
+	nm_ip6_config_merge_setting (config, nm_connection_get_setting_ip6_config (priv->connection));
 
 	priv->ip6_config = config;
 	nm_vpn_connection_config_maybe_complete (connection, TRUE);

src/devices/nm-device.c
@@ -1899,16 +1899,16 @@ static NMIP4Config *
 aipd_get_ip4_config (NMDevice *self, struct in_addr lla)
 {
 	NMIP4Config *config = NULL;
-	NMIP4Address *addr;
+	NMPlatformIP4Address address;
 	NMIP4Route *route;
 
 	config = nm_ip4_config_new ();
 	g_assert (config);
 
-	addr = nm_ip4_address_new ();
-	nm_ip4_address_set_address (addr, (guint32) lla.s_addr);
-	nm_ip4_address_set_prefix (addr, 16);
-	nm_ip4_config_take_address (config, addr);
+	memset (&address, 0, sizeof (address));
+	address.address = lla.s_addr;
+	address.plen = 16;
+	nm_ip4_config_add_address (config, &address);
 
 	/* Add a multicast route for link-local connections: destination= 224.0.0.0, netmask=240.0.0.0 */
 	route = nm_ip4_route_new ();
@@ -2382,7 +2382,7 @@ static NMIP4Config *
 shared4_new_config (NMDevice *self, NMDeviceStateReason *reason)
 {
 	NMIP4Config *config = NULL;
-	NMIP4Address *addr;
+	NMPlatformIP4Address address;
 	guint32 tmp_addr;
 
 	g_return_val_if_fail (self != NULL, NULL);
@@ -2397,10 +2397,10 @@ shared4_new_config (NMDevice *self, NMDeviceStateReason *reason)
 	}
 
 	config = nm_ip4_config_new ();
-	addr = nm_ip4_address_new ();
-	nm_ip4_address_set_address (addr, tmp_addr);
-	nm_ip4_address_set_prefix (addr, 24);
-	nm_ip4_config_take_address (config, addr);
+	memset (&address, 0, sizeof (address));
+	address.address = tmp_addr;
+	address.plen = 24;
+	nm_ip4_config_add_address (config, &address);
 
 	/* Remove the address lock when the object gets disposed */
 	g_object_set_data_full (G_OBJECT (config), "shared-ip",
@@ -3575,7 +3575,7 @@ start_sharing (NMDevice *self, NMIP4Config *config)
 	char str_addr[INET_ADDRSTRLEN + 1];
 	char str_mask[INET_ADDRSTRLEN + 1];
 	guint32 netmask, network;
-	NMIP4Address *ip4_addr;
+	const NMPlatformIP4Address *ip4_addr;
 	const char *ip_iface;
 
 	g_return_val_if_fail (config != NULL, FALSE);
@@ -3583,14 +3583,14 @@ start_sharing (NMDevice *self, NMIP4Config *config)
 	ip_iface = nm_device_get_ip_iface (self);
 
 	ip4_addr = nm_ip4_config_get_address (config, 0);
-	if (!ip4_addr || !nm_ip4_address_get_address (ip4_addr))
+	if (!ip4_addr || !ip4_addr->address)
 		return FALSE;
 
-	netmask = nm_utils_ip4_prefix_to_netmask (nm_ip4_address_get_prefix (ip4_addr));
+	netmask = nm_utils_ip4_prefix_to_netmask (ip4_addr->plen);
 	if (!inet_ntop (AF_INET, &netmask, str_mask, sizeof (str_mask)))
 		return FALSE;
 
-	network = nm_ip4_address_get_address (ip4_addr) & netmask;
+	network = ip4_addr->address & netmask;
 	if (!inet_ntop (AF_INET, &network, str_addr, sizeof (str_addr)))
 		return FALSE;
 
@@ -5998,11 +5998,11 @@ ip4_match_config (NMDevice *self, NMConnection *connection)
 		/* Find at least one lease's address on the device */
 		for (iter = leases; iter; iter = g_slist_next (iter)) {
 			NMIP4Config *ip4_config = iter->data;
-			NMIP4Address *addr = nm_ip4_config_get_address (ip4_config, 0);
+			const NMPlatformIP4Address *address = nm_ip4_config_get_address (ip4_config, 0);
 
-			if (addr && nm_platform_ip4_address_exists (nm_device_get_ip_ifindex (self),
-			                                     nm_ip4_address_get_address (addr),
-			                                     nm_ip4_address_get_prefix (addr))) {
+			if (address && nm_platform_ip4_address_exists (nm_device_get_ip_ifindex (self),
+			                                               address->address,
+			                                               address->plen)) {
 				found = TRUE; /* Yay, device has same address as a lease */
 				break;
 			}

src/dhcp-manager/nm-dhcp-client.c
@@ -1136,9 +1136,9 @@ ip4_options_to_config (NMDHCPClient *self)
 	NMDHCPClientPrivate *priv;
 	NMIP4Config *ip4_config = NULL;
 	struct in_addr tmp_addr;
-	NMIP4Address *addr = NULL;
+	NMPlatformIP4Address address;
 	char *str = NULL;
-	guint32 gwaddr = 0, prefix = 0;
+	guint32 gwaddr = 0, plen = 0;
 
 	g_return_val_if_fail (NM_IS_DHCP_CLIENT (self), NULL);
 
@@ -1146,25 +1146,25 @@ ip4_options_to_config (NMDHCPClient *self)
 	g_return_val_if_fail (priv->options != NULL, NULL);
 
 	ip4_config = nm_ip4_config_new ();
-	addr = nm_ip4_address_new ();
+	memset (&address, 0, sizeof (address));
 
 	str = g_hash_table_lookup (priv->options, "new_ip_address");
 	if (str && (inet_pton (AF_INET, str, &tmp_addr) > 0)) {
-		nm_ip4_address_set_address (addr, tmp_addr.s_addr);
+		address.address = tmp_addr.s_addr;
 		nm_log_info (LOGD_DHCP4, "  address %s", str);
 	} else
 		goto error;
 
 	str = g_hash_table_lookup (priv->options, "new_subnet_mask");
 	if (str && (inet_pton (AF_INET, str, &tmp_addr) > 0)) {
-		prefix = nm_utils_ip4_netmask_to_prefix (tmp_addr.s_addr);
-		nm_log_info (LOGD_DHCP4, "  prefix %d (%s)", prefix, str);
+		plen = nm_utils_ip4_netmask_to_prefix (tmp_addr.s_addr);
+		nm_log_info (LOGD_DHCP4, "  plen %d (%s)", plen, str);
 	} else {
 		/* Get default netmask for the IP according to appropriate class. */
-		prefix = nm_utils_ip4_get_default_prefix (nm_ip4_address_get_address (addr));
-		nm_log_info (LOGD_DHCP4, "  prefix %d (default)", prefix);
+		plen = nm_utils_ip4_get_default_prefix (address.address);
+		nm_log_info (LOGD_DHCP4, "  plen %d (default)", plen);
 	}
-	nm_ip4_address_set_prefix (addr, prefix);
+	address.plen = plen;
 
 	/* Routes: if the server returns classless static routes, we MUST ignore
 	 * the 'static_routes' option.
@@ -1200,8 +1200,7 @@ ip4_options_to_config (NMDHCPClient *self)
 		}
 	}
 
-	nm_ip4_config_take_address (ip4_config, addr);
-	addr = NULL;
+	nm_ip4_config_add_address (ip4_config, &address);
 
 	str = g_hash_table_lookup (priv->options, "new_host_name");
 	if (str)
@@ -1290,8 +1289,6 @@ ip4_options_to_config (NMDHCPClient *self)
 	return ip4_config;
 
 error:
-	if (addr)
-		nm_ip4_address_unref (addr);
 	g_object_unref (ip4_config);
 	return NULL;
 }
@@ -1333,7 +1330,7 @@ ip6_options_to_config (NMDHCPClient *self)
 	NMDHCPClientPrivate *priv;
 	NMIP6Config *ip6_config = NULL;
 	struct in6_addr tmp_addr;
-	NMIP6Address *addr = NULL;
+	NMPlatformIP6Address address;
 	char *str = NULL;
 	GHashTableIter iter;
 	gpointer key, value;
@@ -1359,14 +1356,12 @@ ip6_options_to_config (NMDHCPClient *self)
 			goto error;
 		}
 
-		addr = nm_ip6_address_new ();
-		g_assert (addr);
-		nm_ip6_address_set_address (addr, &tmp_addr);
-		/* DHCPv6 IA_NA assignments are single address only */
-		nm_ip6_address_set_prefix (addr, 128);
-		nm_log_info (LOGD_DHCP6, "  address %s/128", str);
+		memset (&address, 0, sizeof (address));
+		address.address = tmp_addr;
+		address.plen = 128;
+		nm_log_info (LOGD_DHCP6, "  address %s", str);
 
-		nm_ip6_config_take_address (ip6_config, addr);
+		nm_ip6_config_add_address (ip6_config, &address);
 	} else if (priv->info_only == FALSE) {
 		/* No address in Managed mode is a hard error */
 		goto error;

src/dhcp-manager/nm-dhcp-dhclient.c
@@ -236,10 +236,10 @@ nm_dhcp_dhclient_get_lease_config (const char *iface, const char *uuid, gboolean
 
 	for (iter = parsed; iter; iter = g_slist_next (iter)) {
 		NMIP4Config *ip4;
-		NMIP4Address *addr;
+		NMPlatformIP4Address address;
 		const char *data;
 		struct in_addr tmp;
-		guint32 prefix;
+		guint32 plen;
 		struct tm expire;
 
 		hash = iter->data;
@@ -295,14 +295,14 @@ nm_dhcp_dhclient_get_lease_config (const char *iface, const char *uuid, gboolean
 			continue;
 
 		ip4 = nm_ip4_config_new ();
-		addr = nm_ip4_address_new ();
+		memset (&address, 0, sizeof (address));
 
 		/* IP4 address */
 		if (!inet_pton (AF_INET, data, &tmp)) {
 			nm_log_warn (LOGD_DHCP, "couldn't parse DHCP lease file IP4 address '%s'", data);
 			goto error;
 		}
-		nm_ip4_address_set_address (addr, tmp.s_addr);
+		address.address = tmp.s_addr;
 
 		/* Netmask */
 		data = g_hash_table_lookup (hash, "option subnet-mask");
@@ -311,12 +311,12 @@ nm_dhcp_dhclient_get_lease_config (const char *iface, const char *uuid, gboolean
 				nm_log_warn (LOGD_DHCP, "couldn't parse DHCP lease file IP4 subnet mask '%s'", data);
 				goto error;
 			}
-			prefix = nm_utils_ip4_netmask_to_prefix (tmp.s_addr);
+			plen = nm_utils_ip4_netmask_to_prefix (tmp.s_addr);
 		} else {
 			/* Get default netmask for the IP according to appropriate class. */
-			prefix = nm_utils_ip4_get_default_prefix (nm_ip4_address_get_address (addr));
+			plen = nm_utils_ip4_get_default_prefix (address.address);
 		}
-		nm_ip4_address_set_prefix (addr, prefix);
+		address.plen = plen;
 
 		/* Gateway */
 		data = g_hash_table_lookup (hash, "option routers");
@@ -328,12 +328,11 @@ nm_dhcp_dhclient_get_lease_config (const char *iface, const char *uuid, gboolean
 			nm_ip4_config_set_gateway (ip4, tmp.s_addr);
 		}
 
-		nm_ip4_config_take_address (ip4, addr);
+		nm_ip4_config_add_address (ip4, &address);
 		leases = g_slist_append (leases, ip4);
 		continue;
 
 	error:
-		nm_ip4_address_unref (addr);
 		g_object_unref (ip4);
 	}
 

src/dns-manager/nm-dns-utils.c
@@ -79,9 +79,9 @@ nm_dns_utils_get_ip4_rdns_domains (NMIP4Config *ip4)
 	 */
 
 	for (i = 0; i < nm_ip4_config_get_num_addresses (ip4); i++) {
-		NMIP4Address *addr = nm_ip4_config_get_address (ip4, i);
+		const NMPlatformIP4Address *address = nm_ip4_config_get_address (ip4, i);
 
-		add_ip4_to_rdns_array (nm_ip4_address_get_address (addr), domains);
+		add_ip4_to_rdns_array (address->address, domains);
 	}
 
 	for (i = 0; i < nm_ip4_config_get_num_routes (ip4); i++) {

src/dnsmasq-manager/nm-dnsmasq-manager.c
@@ -249,7 +249,7 @@ create_dm_cmd_line (const char *iface,
 	const char *dm_binary;
 	NMCmdLine *cmd;
 	GString *s;
-	NMIP4Address *tmp;
+	const NMPlatformIP4Address *tmp;
 	struct in_addr addr;
 	char buf[INET_ADDRSTRLEN + 15];
 	char localaddr[INET_ADDRSTRLEN + 1];
@@ -294,7 +294,7 @@ create_dm_cmd_line (const char *iface,
 	nm_cmd_line_add_string (cmd, "--strict-order");
 
 	s = g_string_new ("--listen-address=");
-	addr.s_addr = nm_ip4_address_get_address (tmp);
+	addr.s_addr = tmp->address;
 	if (!inet_ntop (AF_INET, &addr, &localaddr[0], INET_ADDRSTRLEN)) {
 		char *err_msg = g_strdup_printf ("error converting IP4 address 0x%X",
 		                                 ntohl (addr.s_addr));
@@ -310,7 +310,7 @@ create_dm_cmd_line (const char *iface,
 	s = g_string_new ("--dhcp-range=");
 
 	/* Add start of address range */
-	addr.s_addr = nm_ip4_address_get_address (tmp) + htonl (9);
+	addr.s_addr = tmp->address + htonl (9);
 	if (!inet_ntop (AF_INET, &addr, &buf[0], INET_ADDRSTRLEN)) {
 		char *err_msg = g_strdup_printf ("error converting IP4 address 0x%X",
 		                                 ntohl (addr.s_addr));
@@ -324,7 +324,7 @@ create_dm_cmd_line (const char *iface,
 	g_string_append_c (s, ',');
 
 	/* Add end of address range */
-	addr.s_addr = nm_ip4_address_get_address (tmp) + htonl (99);
+	addr.s_addr = tmp->address + htonl (99);
 	if (!inet_ntop (AF_INET, &addr, &buf[0], INET_ADDRSTRLEN)) {
 		char *err_msg = g_strdup_printf ("error converting IP4 address 0x%X",
 		                                 ntohl (addr.s_addr));

src/ip6-manager/nm-ip6-manager.c
@@ -1472,8 +1472,7 @@ nm_ip6_manager_get_ip6_config (NMIP6Manager *manager, int ifindex)
 	NMIP6Config *config;
 	struct rtnl_addr *rtnladdr;
 	struct nl_addr *nladdr;
-	struct in6_addr *addr;
-	NMIP6Address *ip6addr;
+	NMPlatformIP6Address address;
 	struct rtnl_route *rtnlroute;
 	struct rtnl_nexthop *nexthop;
 	struct nl_addr *nldest, *nlgateway;
@@ -1567,11 +1566,10 @@ nm_ip6_manager_get_ip6_config (NMIP6Manager *manager, int ifindex)
 		if (!nladdr || nl_addr_get_family (nladdr) != AF_INET6)
 			continue;
 
-		addr = nl_addr_get_binary_addr (nladdr);
-		ip6addr = nm_ip6_address_new ();
-		nm_ip6_address_set_prefix (ip6addr, rtnl_addr_get_prefixlen (rtnladdr));
-		nm_ip6_address_set_address (ip6addr, addr);
-		nm_ip6_config_take_address (config, ip6addr);
+		memset (&address, 0, sizeof (address));
+		address.address = *(struct in6_addr *) nl_addr_get_binary_addr (nladdr);
+		address.plen = rtnl_addr_get_prefixlen (rtnladdr);
+		nm_ip6_config_add_address (config, &address);
 	}
 
 	/* Add DNS servers */

src/modem-manager/nm-modem-broadband.c
@@ -631,10 +631,9 @@ static_stage3_done (NMModemBroadband *self)
 	NMIP4Config *config = NULL;
 	const gchar *address_string;
 	guint32 address_network;
-	NMIP4Address *addr;
+	NMPlatformIP4Address address;
 	const gchar **dns;
 	guint i;
-	guint prefix;
 
 	g_assert (self->priv->ipv4_config);
 
@@ -653,12 +652,11 @@ static_stage3_done (NMModemBroadband *self)
 	}
 
 	config = nm_ip4_config_new ();
-	addr = nm_ip4_address_new ();
-	nm_ip4_address_set_address (addr, address_network);
-	prefix = mm_bearer_ip_config_get_prefix (self->priv->ipv4_config);
-	if (prefix > 0)
-		nm_ip4_address_set_prefix (addr, prefix);
-	nm_ip4_config_take_address (config, addr);
+
+	memset (&address, 0, sizeof (address));
+	address.address = address_network;
+	address.plen = mm_bearer_ip_config_get_prefix (self->priv->ipv4_config);
+	nm_ip4_config_add_address (config, &address);
 
 	nm_log_info (LOGD_MB, "  address %s/%d",
 	             mm_bearer_ip_config_get_address (self->priv->ipv4_config),

commit 4d33ac21dc9f21c1e281feef4157ef2d467db5a5

vpn: fix zero IPv6 address check
Bug introduced by commit d8e6065.

https://bugzilla.gnome.org/show_bug.cgi?id=706286

Reported-by: Nicolas Iooss <nicolas.iooss.2010_nm@m4x.org>
Acked-by: Thomas Haller <thaller@redhat.com>

src/vpn-manager/nm-vpn-connection.c
@@ -1028,7 +1028,7 @@ nm_vpn_connection_ip6_config_get (DBusGProxy *proxy,
 	if (val)
 		address.plen = g_value_get_uint (val);
 
-	if (IN6_IS_ADDR_UNSPECIFIED (&address.address) && address.plen)
+	if (!IN6_IS_ADDR_UNSPECIFIED (&address.address) && address.plen)
 		nm_ip6_config_add_address (config, &address);
 	else {
 		nm_log_err (LOGD_VPN, "invalid IP6 config received!");