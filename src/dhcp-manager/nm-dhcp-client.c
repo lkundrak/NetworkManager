lkundrak/NetworkManager

commit 53e55aab365a263df5d2e13efe28a5dc5a7b8432

dhcp: push address lifetime to nm-ip[46]-config

src/dhcp-manager/nm-dhcp-client.c
@@ -800,6 +800,16 @@ nm_dhcp_client_foreach_option (NMDHCPClient *self,
 	return TRUE;
 }
 
+static guint32
+get_time (void)
+{
+	struct timespec tp;
+
+	clock_gettime (CLOCK_MONOTONIC, &tp);
+
+	return tp.tv_sec;
+}
+
 /********************************************/
 
 static gboolean
@@ -1142,6 +1152,8 @@ ip4_options_to_config (NMDHCPClient *self)
 
 	g_return_val_if_fail (NM_IS_DHCP_CLIENT (self), NULL);
 
+	address.timestamp = get_time ();
+
 	priv = NM_DHCP_CLIENT_GET_PRIVATE (self);
 	g_return_val_if_fail (priv->options != NULL, NULL);
 
@@ -1198,6 +1210,12 @@ ip4_options_to_config (NMDHCPClient *self)
 			}
 			g_strfreev (routers);
 		}
+	}
+
+	str = g_hash_table_lookup (priv->options, "new_dhcp_lease_time");
+	if (str) {
+		address.lifetime = address.preferred = strtoul (str, NULL, 10);
+		nm_log_info (LOGD_DHCP4, "  lease time %d", address.lifetime);
 	}
 
 	nm_ip4_config_add_address (ip4_config, &address);
@@ -1337,6 +1355,9 @@ ip6_options_to_config (NMDHCPClient *self)
 
 	g_return_val_if_fail (NM_IS_DHCP_CLIENT (self), NULL);
 
+	address.plen = 128;
+	address.timestamp = get_time ();
+
 	priv = NM_DHCP_CLIENT_GET_PRIVATE (self);
 	g_return_val_if_fail (priv->options != NULL, NULL);
 
@@ -1358,14 +1379,20 @@ ip6_options_to_config (NMDHCPClient *self)
 
 		memset (&address, 0, sizeof (address));
 		address.address = tmp_addr;
-		address.plen = 128;
 		nm_log_info (LOGD_DHCP6, "  address %s", str);
 
-		nm_ip6_config_add_address (ip6_config, &address);
 	} else if (priv->info_only == FALSE) {
 		/* No address in Managed mode is a hard error */
 		goto error;
 	}
+
+	str = g_hash_table_lookup (priv->options, "new_dhcp_lease_time");
+	if (str) {
+		address.lifetime = address.preferred = strtoul (str, NULL, 10);
+		nm_log_info (LOGD_DHCP6, "  lease time %d", address.lifetime);
+	}
+
+	nm_ip6_config_add_address (ip6_config, &address);
 
 	str = g_hash_table_lookup (priv->options, "new_host_name");
 	if (str)

commit 0b450b0b99e92e6579b947877dd9d3166078c016

dhcp: fix IPv6 address prefixes (rh #1013583)
53e55aab365a263df5d2e13efe28a5dc5a7b8432 mistakenly moved the address
prefix without moving the memset() to initialize the address.  Make
sure the address is initialized before trying to do anything with it.

src/dhcp-manager/nm-dhcp-client.c
@@ -1348,6 +1348,7 @@ ip6_options_to_config (NMDHCPClient *self)
 
 	g_return_val_if_fail (NM_IS_DHCP_CLIENT (self), NULL);
 
+	memset (&address, 0, sizeof (address));
 	address.plen = 128;
 	address.timestamp = get_time ();
 
@@ -1370,7 +1371,6 @@ ip6_options_to_config (NMDHCPClient *self)
 			goto error;
 		}
 
-		memset (&address, 0, sizeof (address));
 		address.address = tmp_addr;
 		nm_log_info (LOGD_DHCP6, "  address %s", str);
 