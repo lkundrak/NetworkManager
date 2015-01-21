lkundrak/NetworkManager

commit 583568e12f9e580cd2903811637c9f9b7a2f1088

dns-manager: don't replace /etc/resolv.conf installed by other tools
Resolves:

 * https://bugzilla.gnome.org/show_bug.cgi?id=732941
 * https://bugzilla.redhat.com/show_bug.cgi?id=1116999

Acked-By: Dan Williams <dcbw@redhat.com>
Acked-By: Thomas Haller <thaller@redhat.com>

src/dns-manager/nm-dns-manager.c
@@ -27,6 +27,7 @@
 #include <fcntl.h>
 #include <resolv.h>
 #include <stdlib.h>
+#include <sys/stat.h>
 #include <sys/ioctl.h>
 #include <unistd.h>
 
@@ -446,6 +447,7 @@ update_resolv_conf (char **searches,
                     GError **error)
 {
 	FILE *f;
+	struct stat st;
 
 	g_return_val_if_fail (error != NULL, FALSE);
 
@@ -484,6 +486,39 @@ update_resolv_conf (char **searches,
 		             NM_MANAGER_ERROR_FAILED,
 		             "Could not replace %s: %s\n",
 		             MY_RESOLV_CONF,
+		             g_strerror (errno));
+		return FALSE;
+	}
+
+	/* Don't overwrite a symbolic link unless it points to MY_RESOLV_CONF. */
+	if (lstat (_PATH_RESCONF, &st) != -1) {
+		/* Don't overwrite a symbolic link. */
+		if (S_ISLNK (st.st_mode)) {
+			if (stat (_PATH_RESCONF, &st) != -1) {
+				char *path = g_file_read_link (_PATH_RESCONF, NULL);
+				gboolean not_ours = g_strcmp0 (path, MY_RESOLV_CONF) != 0;
+
+				g_free (path);
+				if (not_ours)
+					return TRUE;
+			} else {
+				if (errno != ENOENT)
+					return TRUE;
+				g_set_error (error,
+							 NM_MANAGER_ERROR,
+							 NM_MANAGER_ERROR_FAILED,
+							 "Could not stat %s: %s\n",
+							 _PATH_RESCONF,
+							 g_strerror (errno));
+				return FALSE;
+			}
+		}
+	} else if (errno != ENOENT) {
+		g_set_error (error,
+		             NM_MANAGER_ERROR,
+		             NM_MANAGER_ERROR_FAILED,
+		             "Could not lstat %s: %s\n",
+		             _PATH_RESCONF,
 		             g_strerror (errno));
 		return FALSE;
 	}

commit 0e3600e25ea8b38eb8422b664efb5c14a91d2b3a

trival/whitespace: fix indention in nm-dns-manager.c
Fixes: 583568e12f9e580cd2903811637c9f9b7a2f1088

src/dns-manager/nm-dns-manager.c
@@ -491,11 +491,11 @@ update_resolv_conf (char **searches,
 				if (errno != ENOENT)
 					return TRUE;
 				g_set_error (error,
-							 NM_MANAGER_ERROR,
-							 NM_MANAGER_ERROR_FAILED,
-							 "Could not stat %s: %s\n",
-							 _PATH_RESCONF,
-							 g_strerror (errno));
+				             NM_MANAGER_ERROR,
+				             NM_MANAGER_ERROR_FAILED,
+				             "Could not stat %s: %s\n",
+				             _PATH_RESCONF,
+				             g_strerror (errno));
 				return FALSE;
 			}
 		}