lkundrak/NetworkManager

commit 388b7830f322b60960884328ff51f7b4df0ef3d3

platform: don't wait for udev before announcing links

src/platform/nm-fake-platform.c
@@ -116,6 +116,7 @@ link_init (NMFakePlatformLink *device, int ifindex, int type, const char *name)
 	device->link.type_name = type_to_type_name (type);
 	device->link.driver = type_to_type_name (type);
 	device->link.udi = device->udi = g_strdup_printf ("fake:%d", ifindex);
+	device->link.initialized = TRUE;
 	if (name)
 		strcpy (device->link.name, name);
 	switch (device->link.type) {

src/devices/nm-device-ethernet.c
@@ -1616,6 +1616,17 @@ carrier_changed (NMDevice *device, gboolean carrier)
 }
 
 static void
+link_changed (NMDevice *device, NMPlatformLink *info)
+{
+	NMDeviceEthernet *self = NM_DEVICE_ETHERNET (device);
+	NMDeviceEthernetPrivate *priv = NM_DEVICE_ETHERNET_GET_PRIVATE (self);
+
+	NM_DEVICE_CLASS (nm_device_ethernet_parent_class)->link_changed (device, info);
+	if (!priv->subchan1 && info->udi)
+		_update_s390_subchannels (self);
+}
+
+static void
 dispose (GObject *object)
 {
 	NMDeviceEthernet *self = NM_DEVICE_ETHERNET (object);
@@ -1714,6 +1725,7 @@ nm_device_ethernet_class_init (NMDeviceEthernetClass *klass)
 	parent_class->spec_match_list = spec_match_list;
 	parent_class->update_connection = update_connection;
 	parent_class->carrier_changed = carrier_changed;
+	parent_class->link_changed = link_changed;
 
 	parent_class->state_changed = device_state_changed;
 

src/devices/nm-device.c
@@ -176,6 +176,7 @@ typedef struct {
 typedef struct {
 	gboolean in_state_changed;
 	gboolean initialized;
+	gboolean platform_link_initialized;
 
 	NMDeviceState state;
 	NMDeviceStateReason state_reason;
@@ -1051,6 +1052,7 @@ void
 nm_device_finish_init (NMDevice *self)
 {
 	NMDevicePrivate *priv = NM_DEVICE_GET_PRIVATE (self);
+	gboolean platform_unmanaged = FALSE;
 
 	g_assert (priv->initialized == FALSE);
 
@@ -1062,6 +1064,20 @@ nm_device_finish_init (NMDevice *self)
 
 	if (priv->master)
 		nm_device_enslave_slave (priv->master, self, NULL);
+
+	if (priv->ifindex > 0) {
+		if (priv->platform_link_initialized || (priv->is_nm_owned && priv->is_software)) {
+			nm_platform_link_get_unmanaged (NM_PLATFORM_GET, priv->ifindex, &platform_unmanaged);
+			nm_device_set_initial_unmanaged_flag (self, NM_UNMANAGED_DEFAULT, platform_unmanaged);
+		} else {
+			/* Hardware and externally-created software links stay unmanaged
+			 * until they are fully initialized by the platform. NM created
+			 * links must be available for activation immediately and thus
+			 * do not get the PLATFORM_INIT unmanaged flag set.
+			 */
+			nm_device_set_initial_unmanaged_flag (self, NM_UNMANAGED_PLATFORM_INIT, TRUE);
+		}
+	}
 
 	priv->initialized = TRUE;
 }
@@ -1255,12 +1271,20 @@ device_link_changed (NMDevice *self, NMPlatformLink *info)
 	NMDevicePrivate *priv = NM_DEVICE_GET_PRIVATE (self);
 	NMUtilsIPv6IfaceId token_iid;
 	gboolean ip_ifname_changed = FALSE;
+	gboolean platform_unmanaged = FALSE;
 
 	if (info->udi && g_strcmp0 (info->udi, priv->udi)) {
 		/* Update UDI to what udev gives us */
 		g_free (priv->udi);
 		priv->udi = g_strdup (info->udi);
 		g_object_notify (G_OBJECT (self), NM_DEVICE_UDI);
+	}
+
+	if (g_strcmp0 (info->driver, priv->driver)) {
+		/* Update driver to what udev gives us */
+		g_free (priv->driver);
+		priv->driver = g_strdup (info->driver);
+		g_object_notify (G_OBJECT (self), NM_DEVICE_DRIVER);
 	}
 
 	/* Update MTU if it has changed. */
@@ -1354,6 +1378,22 @@ device_link_changed (NMDevice *self, NMPlatformLink *info)
 				                         NM_DEVICE_STATE_REASON_USER_REQUESTED);
 			}
 		}
+	}
+
+	if (priv->ifindex > 0 && !priv->platform_link_initialized && info->initialized) {
+		priv->platform_link_initialized = TRUE;
+
+		if (nm_platform_link_get_unmanaged (NM_PLATFORM_GET, priv->ifindex, &platform_unmanaged)) {
+			nm_device_set_unmanaged (self,
+			                         NM_UNMANAGED_DEFAULT,
+			                         platform_unmanaged,
+			                         NM_DEVICE_STATE_REASON_USER_REQUESTED);
+		}
+
+		nm_device_set_unmanaged (self,
+		                         NM_UNMANAGED_PLATFORM_INIT,
+		                         FALSE,
+		                         NM_DEVICE_STATE_REASON_NOW_MANAGED);
 	}
 }
 
@@ -7088,7 +7128,7 @@ nm_device_set_unmanaged (NMDevice *self,
 
 		if (unmanaged)
 			nm_device_state_changed (self, NM_DEVICE_STATE_UNMANAGED, reason);
-		else
+		else if (nm_device_get_state (self) == NM_DEVICE_STATE_UNMANAGED)
 			nm_device_state_changed (self, NM_DEVICE_STATE_UNAVAILABLE, reason);
 	}
 }
@@ -8716,6 +8756,7 @@ set_property (GObject *object, guint prop_id,
 			priv->up = platform_device->up;
 			g_free (priv->driver);
 			priv->driver = g_strdup (platform_device->driver);
+			priv->platform_link_initialized = platform_device->initialized;
 		}
 		break;
 	case PROP_UDI:

src/devices/nm-device.h
@@ -344,6 +344,8 @@ RfKillType nm_device_get_rfkill_type (NMDevice *device);
  * @NM_UNMANAGED_USER: %TRUE when unmanaged by user decision (via unmanaged-specs)
  * @NM_UNMANAGED_PARENT: %TRUE when unmanaged due to parent device being unmanaged
  * @NM_UNMANAGED_EXTERNAL_DOWN: %TRUE when unmanaged because !IFF_UP and not created by NM
+ * @NM_UNMANAGED_PLATFORM_INIT: %TRUE when unmanaged because platform link not
+ *   yet initialized
  */
 typedef enum {
 	NM_UNMANAGED_NONE          = 0x00,
@@ -352,6 +354,7 @@ typedef enum {
 	NM_UNMANAGED_USER          = 0x04,
 	NM_UNMANAGED_PARENT        = 0x08,
 	NM_UNMANAGED_EXTERNAL_DOWN = 0x10,
+	NM_UNMANAGED_PLATFORM_INIT = 0x20,
 
 	/* Boundary value */
 	__NM_UNMANAGED_LAST,

src/nm-manager.c
@@ -1791,7 +1791,7 @@ add_device (NMManager *self, NMDevice *device, gboolean try_assume)
 	NMManagerPrivate *priv = NM_MANAGER_GET_PRIVATE (self);
 	const char *iface, *driver, *type_desc;
 	const GSList *unmanaged_specs;
-	gboolean user_unmanaged, sleeping, platform_unmanaged;
+	gboolean user_unmanaged, sleeping;
 	gboolean enabled = FALSE;
 	RfKillType rtype;
 	GSList *iter, *remove = NULL;
@@ -1870,9 +1870,6 @@ add_device (NMManager *self, NMDevice *device, gboolean try_assume)
 	unmanaged_specs = nm_settings_get_unmanaged_specs (priv->settings);
 	user_unmanaged = nm_device_spec_match_list (device, unmanaged_specs);
 	nm_device_set_initial_unmanaged_flag (device, NM_UNMANAGED_USER, user_unmanaged);
-
-	if (nm_platform_link_get_unmanaged (NM_PLATFORM_GET, nm_device_get_ifindex (device), &platform_unmanaged))
-		nm_device_set_initial_unmanaged_flag (device, NM_UNMANAGED_DEFAULT, platform_unmanaged);
 
 	sleeping = manager_sleeping (self);
 	nm_device_set_initial_unmanaged_flag (device, NM_UNMANAGED_INTERNAL, sleeping);

src/platform/nm-linux-platform.c
@@ -887,19 +887,6 @@ type_to_string (NMLinkType type)
 	}
 }
 
-static gboolean
-link_is_announceable (NMPlatform *platform, struct rtnl_link *rtnllink)
-{
-	NMLinuxPlatformPrivate *priv = NM_LINUX_PLATFORM_GET_PRIVATE (platform);
-
-	/* Hardware devices must be found by udev so rules get run and tags set */
-	if (g_hash_table_lookup (priv->udev_devices,
-	                         GINT_TO_POINTER (rtnl_link_get_ifindex (rtnllink))))
-		return TRUE;
-
-	return FALSE;
-}
-
 #define DEVTYPE_PREFIX "DEVTYPE="
 
 static char *
@@ -1058,14 +1045,16 @@ init_link (NMPlatform *platform, NMPlatformLink *info, struct rtnl_link *rtnllin
 	udev_device = g_hash_table_lookup (priv->udev_devices, GINT_TO_POINTER (info->ifindex));
 	if (udev_device) {
 		info->driver = udev_get_driver (udev_device, info->ifindex);
-		if (!info->driver)
-			info->driver = g_intern_string (rtnl_link_get_type (rtnllink));
-		if (!info->driver)
-			info->driver = ethtool_get_driver (info->name);
-		if (!info->driver)
-			info->driver = "unknown";
 		info->udi = g_udev_device_get_sysfs_path (udev_device);
+		info->initialized = TRUE;
 	}
+
+	if (!info->driver)
+		info->driver = g_intern_string (rtnl_link_get_type (rtnllink));
+	if (!info->driver)
+		info->driver = ethtool_get_driver (info->name);
+	if (!info->driver)
+		info->driver = "unknown";
 
 	return TRUE;
 }
@@ -1624,22 +1613,6 @@ announce_object (NMPlatform *platform, const struct nl_object *object, NMPlatfor
 			if (!init_link (platform, &device, rtnl_link))
 				return;
 
-			/* Skip devices not yet discovered by udev. They will be
-			 * announced by udev_device_added(). This doesn't apply to removed
-			 * devices, as those come either from udev_device_removed(),
-			 * event_notification() or link_delete() which block the announcment
-			 * themselves when appropriate.
-			 */
-			switch (change_type) {
-			case NM_PLATFORM_SIGNAL_ADDED:
-			case NM_PLATFORM_SIGNAL_CHANGED:
-				if (!device.driver)
-					return;
-				break;
-			default:
-				break;
-			}
-
 			/* Link deletion or setting down is sometimes accompanied by address
 			 * and/or route deletion.
 			 *
@@ -2067,15 +2040,12 @@ event_notification (struct nl_msg *msg, gpointer user_data)
 			return NL_OK;
 
 		nl_cache_remove (cached_object);
-		/* Don't announce removed interfaces that are not recognized by
-		 * udev. They were either not yet discovered or they have been
-		 * already removed and announced.
-		 */
-		if (event == RTM_DELLINK) {
-			if (!link_is_announceable (platform, (struct rtnl_link *) cached_object))
-				return NL_OK;
-		}
 		announce_object (platform, cached_object, NM_PLATFORM_SIGNAL_REMOVED, NM_PLATFORM_REASON_EXTERNAL);
+		if (event == RTM_DELLINK) {
+			int ifindex = rtnl_link_get_ifindex ((struct rtnl_link *) cached_object);
+
+			g_hash_table_remove (priv->udev_devices, GINT_TO_POINTER (ifindex));
+		}
 
 		return NL_OK;
 	case RTM_NEWLINK:
@@ -2303,12 +2273,8 @@ link_get_all (NMPlatform *platform)
 	struct nl_object *object;
 
 	for (object = nl_cache_get_first (priv->link_cache); object; object = nl_cache_get_next (object)) {
-		struct rtnl_link *rtnl_link = (struct rtnl_link *) object;
-
-		if (link_is_announceable (platform, rtnl_link)) {
-			if (init_link (platform, &device, rtnl_link))
-				g_array_append_val (links, device);
-		}
+		if (init_link (platform, &device, (struct rtnl_link *) object))
+			g_array_append_val (links, device);
 	}
 
 	return links;
@@ -2321,13 +2287,7 @@ _nm_platform_link_get (NMPlatform *platform, int ifindex, NMPlatformLink *l)
 	auto_nl_object struct rtnl_link *rtnllink = NULL;
 
 	rtnllink = rtnl_link_get (priv->link_cache, ifindex);
-	if (rtnllink) {
-		if (link_is_announceable (platform, rtnllink)) {
-			if (init_link (platform, l, rtnllink))
-				return TRUE;
-		}
-	}
-	return FALSE;
+	return (rtnllink && init_link (platform, l, rtnllink));
 }
 
 static struct nl_object *
@@ -2383,13 +2343,6 @@ link_get (NMPlatform *platform, int ifindex)
 
 	if (!rtnllink) {
 		platform->error = NM_PLATFORM_ERROR_NOT_FOUND;
-		return NULL;
-	}
-
-	/* physical interfaces must be found by udev before they can be used */
-	if (!link_is_announceable (platform, rtnllink)) {
-		platform->error = NM_PLATFORM_ERROR_NOT_FOUND;
-		rtnl_link_put (rtnllink);
 		return NULL;
 	}
 
@@ -4432,7 +4385,6 @@ udev_device_added (NMPlatform *platform,
 	auto_nl_object struct rtnl_link *rtnllink = NULL;
 	const char *ifname;
 	int ifindex;
-	gboolean was_announceable = FALSE;
 
 	ifname = g_udev_device_get_name (udev_device);
 	if (!ifname) {
@@ -4457,15 +4409,15 @@ udev_device_added (NMPlatform *platform,
 	}
 
 	rtnllink = rtnl_link_get (priv->link_cache, ifindex);
-	if (rtnllink)
-		was_announceable = link_is_announceable (platform, rtnllink);
+	if (!rtnllink) {
+		warning ("(%s): udev-add: interface not known via netlink; ignoring...", ifname);
+		return;
+	}
 
 	g_hash_table_insert (priv->udev_devices, GINT_TO_POINTER (ifindex),
 	                     g_object_ref (udev_device));
 
-	/* Announce devices only if they also have been discovered via Netlink. */
-	if (rtnllink && link_is_announceable (platform, rtnllink))
-		announce_object (platform, (struct nl_object *) rtnllink, was_announceable ? NM_PLATFORM_SIGNAL_CHANGED : NM_PLATFORM_SIGNAL_ADDED, NM_PLATFORM_REASON_EXTERNAL);
+	announce_object (platform, (struct nl_object *) rtnllink, NM_PLATFORM_SIGNAL_CHANGED, NM_PLATFORM_REASON_EXTERNAL);
 }
 
 static void
@@ -4473,9 +4425,7 @@ udev_device_removed (NMPlatform *platform,
                      GUdevDevice *udev_device)
 {
 	NMLinuxPlatformPrivate *priv = NM_LINUX_PLATFORM_GET_PRIVATE (platform);
-	auto_nl_object struct rtnl_link *rtnllink = NULL;
 	int ifindex = 0;
-	gboolean was_announceable = FALSE;
 
 	if (g_udev_device_get_property (udev_device, "IFINDEX"))
 		ifindex = g_udev_device_get_property_as_int (udev_device, "IFINDEX");
@@ -4500,15 +4450,7 @@ udev_device_removed (NMPlatform *platform,
 	if (ifindex <= 0)
 		return;
 
-	rtnllink = rtnl_link_get (priv->link_cache, ifindex);
-	if (rtnllink)
-		was_announceable = link_is_announceable (platform, rtnllink);
-
 	g_hash_table_remove (priv->udev_devices, GINT_TO_POINTER (ifindex));
-
-	/* Announce device removal if it is no longer announceable. */
-	if (was_announceable && !link_is_announceable (platform, rtnllink))
-		announce_object (platform, (struct nl_object *) rtnllink, NM_PLATFORM_SIGNAL_REMOVED, NM_PLATFORM_REASON_EXTERNAL);
 }
 
 static void
@@ -4553,8 +4495,6 @@ constructed (GObject *_object)
 	NMPlatform *platform = NM_PLATFORM (_object);
 	NMLinuxPlatformPrivate *priv = NM_LINUX_PLATFORM_GET_PRIVATE (platform);
 	const char *udev_subsys[] = { "net", NULL };
-	GUdevEnumerator *enumerator;
-	GList *devices, *iter;
 	int channel_flags;
 	gboolean status;
 	int nle;
@@ -4614,6 +4554,24 @@ constructed (GObject *_object)
 	g_signal_connect (priv->udev_client, "uevent", G_CALLBACK (handle_udev_event), platform);
 	priv->udev_devices = g_hash_table_new_full (NULL, NULL, NULL, g_object_unref);
 
+	/* request all IPv6 addresses (hopeing that there is at least one), to check for
+	 * the IFA_FLAGS attribute. */
+	nle = nl_rtgen_request (priv->nlh_event, RTM_GETADDR, AF_INET6, NLM_F_DUMP);
+	if (nle < 0)
+		nm_log_warn (LOGD_PLATFORM, "Netlink error: requesting RTM_GETADDR failed with %s", nl_geterror (nle));
+
+	priv->wifi_data = g_hash_table_new_full (NULL, NULL, NULL, (GDestroyNotify) wifi_utils_deinit);
+
+	G_OBJECT_CLASS (nm_linux_platform_parent_class)->constructed (_object);
+}
+
+static void
+setup_devices (NMPlatform *platform)
+{
+	NMLinuxPlatformPrivate *priv = NM_LINUX_PLATFORM_GET_PRIVATE (platform);
+	GUdevEnumerator *enumerator;
+	GList *devices, *iter;
+
 	/* And read initial device list */
 	enumerator = g_udev_enumerator_new (priv->udev_client);
 	g_udev_enumerator_add_match_subsystem (enumerator, "net");
@@ -4631,16 +4589,6 @@ constructed (GObject *_object)
 	}
 	g_list_free (devices);
 	g_object_unref (enumerator);
-
-	/* request all IPv6 addresses (hopeing that there is at least one), to check for
-	 * the IFA_FLAGS attribute. */
-	nle = nl_rtgen_request (priv->nlh_event, RTM_GETADDR, AF_INET6, NLM_F_DUMP);
-	if (nle < 0)
-		nm_log_warn (LOGD_PLATFORM, "Netlink error: requesting RTM_GETADDR failed with %s", nl_geterror (nle));
-
-	priv->wifi_data = g_hash_table_new_full (NULL, NULL, NULL, (GDestroyNotify) wifi_utils_deinit);
-
-	G_OBJECT_CLASS (nm_linux_platform_parent_class)->constructed (_object);
 }
 
 static void
@@ -4677,6 +4625,8 @@ nm_linux_platform_class_init (NMLinuxPlatformClass *klass)
 	/* virtual methods */
 	object_class->constructed = constructed;
 	object_class->finalize = nm_linux_platform_finalize;
+
+	platform_class->setup_devices = setup_devices;
 
 	platform_class->sysctl_set = sysctl_set;
 	platform_class->sysctl_get = sysctl_get;

src/platform/nm-platform.c
@@ -405,6 +405,10 @@ nm_platform_query_devices (NMPlatform *self)
 		               NM_PLATFORM_REASON_INTERNAL);
 	}
 	g_array_unref (links_array);
+
+	/* Platform specific device setup. */
+	if (klass->setup_devices)
+		klass->setup_devices (self);
 }
 
 /**
@@ -2608,6 +2612,12 @@ nm_platform_ip6_route_to_string (const NMPlatformIP6Route *route)
             return (((a)->field) < ((b)->field)) ? -1 : 1;  \
     } G_STMT_END
 
+#define _CMP_FIELD_BOOL(a, b, field)                        \
+    G_STMT_START {                                          \
+        if ((!((a)->field)) != (!((b)->field)))                 \
+            return ((!((a)->field)) < (!((b)->field))) ? -1 : 1; \
+    } G_STMT_END
+
 #define _CMP_FIELD_STR(a, b, field)                         \
     G_STMT_START {                                          \
         int c = strcmp ((a)->field, (b)->field);            \
@@ -2643,6 +2653,7 @@ nm_platform_link_cmp (const NMPlatformLink *a, const NMPlatformLink *b)
 	_CMP_FIELD (a, b, arp);
 	_CMP_FIELD (a, b, mtu);
 	_CMP_FIELD_STR0 (a, b, type_name);
+	_CMP_FIELD_BOOL (a, b, initialized);
 	_CMP_FIELD_STR0 (a, b, udi);
 	_CMP_FIELD_STR0 (a, b, driver);
 	return 0;

src/platform/nm-platform.h
@@ -88,6 +88,7 @@ struct _NMPlatformLink {
 	const char *type_name;
 	const char *udi;
 	const char *driver;
+	gboolean initialized;
 	int master;
 	int parent;
 	gboolean up;
@@ -357,6 +358,8 @@ struct _NMPlatform {
 
 typedef struct {
 	GObjectClass parent;
+
+	void (*setup_devices) (NMPlatform *);
 
 	gboolean (*sysctl_set) (NMPlatform *, const char *path, const char *value);
 	char * (*sysctl_get) (NMPlatform *, const char *path);

commit 5134fb1ebf5fd1d30dbad0b1e0fdcf3edf981ed5

platform: fix root-tests after adding link detection without udev
Co-Authored-By: Lubomir Rintel <lkundrak@v3.sk>

Fixes: 388b7830f322b60960884328ff51f7b4df0ef3d3

src/platform/nm-fake-platform.c
@@ -49,11 +49,6 @@ typedef struct {
 	int vlan_id;
 } NMFakePlatformLink;
 
-typedef struct {
-	int ifindex;
-	NMPlatform *platform;
-} NMFakePlatformLinkData;
-
 #define NM_FAKE_PLATFORM_GET_PRIVATE(o) (G_TYPE_INSTANCE_GET_PRIVATE ((o), NM_TYPE_FAKE_PLATFORM, NMFakePlatformPrivate))
 
 G_DEFINE_TYPE (NMFakePlatform, nm_fake_platform, NM_TYPE_PLATFORM)
@@ -173,23 +168,6 @@ _nm_platform_link_get (NMPlatform *platform, int ifindex, NMPlatformLink *l)
 }
 
 static gboolean
-_link_announce (NMFakePlatformLinkData *data)
-{
-	NMFakePlatformLink *device = link_get (data->platform, data->ifindex);
-
-	if (device)
-		g_signal_emit_by_name (data->platform,
-				       NM_PLATFORM_SIGNAL_LINK_CHANGED,
-				       device->link.ifindex,
-				       device,
-				       NM_PLATFORM_SIGNAL_ADDED,
-				       NM_PLATFORM_REASON_INTERNAL);
-	g_free (data);
-
-	return FALSE;
-}
-
-static gboolean
 link_add (NMPlatform *platform, const char *name, NMLinkType type, const void *address, size_t address_len)
 {
 	NMFakePlatformPrivate *priv = NM_FAKE_PLATFORM_GET_PRIVATE (platform);
@@ -199,12 +177,8 @@ link_add (NMPlatform *platform, const char *name, NMLinkType type, const void *a
 
 	g_array_append_val (priv->links, device);
 
-	if (device.link.ifindex) {
-		NMFakePlatformLinkData *data = g_new (NMFakePlatformLinkData, 1);
-		data->ifindex = device.link.ifindex;
-		data->platform = platform;
-		g_idle_add ((GSourceFunc) _link_announce, data);
-	}
+	if (device.link.ifindex)
+		g_signal_emit_by_name (platform, NM_PLATFORM_SIGNAL_LINK_CHANGED, device.link.ifindex, &device, NM_PLATFORM_SIGNAL_ADDED, NM_PLATFORM_REASON_INTERNAL);
 
 	return TRUE;
 }

src/platform/tests/test-address.c
@@ -260,7 +260,7 @@ setup_tests (void)
 	nm_platform_link_delete (NM_PLATFORM_GET, nm_platform_link_get_ifindex (NM_PLATFORM_GET, DEVICE_NAME));
 	g_assert (!nm_platform_link_exists (NM_PLATFORM_GET, DEVICE_NAME));
 	g_assert (nm_platform_dummy_add (NM_PLATFORM_GET, DEVICE_NAME));
-	wait_signal (link_added);
+	accept_signal (link_added);
 	free_signal (link_added);
 
 	g_test_add_func ("/address/internal/ip4", test_ip4_address);

src/platform/tests/test-cleanup.c
@@ -36,7 +36,7 @@ test_cleanup_internal (void)
 
 	/* Create and set up device */
 	g_assert (nm_platform_dummy_add (NM_PLATFORM_GET, DEVICE_NAME));
-	wait_signal (link_added);
+	accept_signal (link_added);
 	free_signal (link_added);
 	g_assert (nm_platform_link_set_up (NM_PLATFORM_GET, nm_platform_link_get_ifindex (NM_PLATFORM_GET, DEVICE_NAME)));
 	ifindex = nm_platform_link_get_ifindex (NM_PLATFORM_GET, DEVICE_NAME);

src/platform/tests/test-link.c
@@ -115,7 +115,7 @@ software_add (NMLinkType link_type, const char *name)
 		/* Don't call link_callback for the bridge interface */
 		parent_added = add_signal_ifname (NM_PLATFORM_SIGNAL_LINK_CHANGED, NM_PLATFORM_SIGNAL_ADDED, link_callback, PARENT_NAME);
 		if (nm_platform_bridge_add (NM_PLATFORM_GET, PARENT_NAME, NULL, 0))
-			wait_signal (parent_added);
+			accept_signal (parent_added);
 		free_signal (parent_added);
 
 		{
@@ -148,7 +148,7 @@ test_slave (int master, int type, SignalData *master_changed)
 	g_assert (ifindex > 0);
 	link_changed = add_signal_ifindex (NM_PLATFORM_SIGNAL_LINK_CHANGED, NM_PLATFORM_SIGNAL_CHANGED, link_callback, ifindex);
 	link_removed = add_signal_ifindex (NM_PLATFORM_SIGNAL_LINK_CHANGED, NM_PLATFORM_SIGNAL_REMOVED, link_callback, ifindex);
-	wait_signal (link_added);
+	accept_signal (link_added);
 
 	/* Set the slave up to see whether master's IFF_LOWER_UP is set correctly.
 	 *
@@ -263,7 +263,7 @@ test_software (NMLinkType link_type, const char *link_typename)
 	link_added = add_signal_ifname (NM_PLATFORM_SIGNAL_LINK_CHANGED, NM_PLATFORM_SIGNAL_ADDED, link_callback, DEVICE_NAME);
 	g_assert (software_add (link_type, DEVICE_NAME));
 	no_error ();
-	wait_signal (link_added);
+	accept_signal (link_added);
 	g_assert (nm_platform_link_exists (NM_PLATFORM_GET, DEVICE_NAME));
 	ifindex = nm_platform_link_get_ifindex (NM_PLATFORM_GET, DEVICE_NAME);
 	g_assert (ifindex >= 0);
@@ -405,7 +405,7 @@ test_internal (void)
 	/* Add device */
 	g_assert (nm_platform_dummy_add (NM_PLATFORM_GET, DEVICE_NAME));
 	no_error ();
-	wait_signal (link_added);
+	accept_signal (link_added);
 
 	/* Try to add again */
 	g_assert (!nm_platform_dummy_add (NM_PLATFORM_GET, DEVICE_NAME));
@@ -485,6 +485,7 @@ test_external (void)
 
 	run_command ("ip link add %s type %s", DEVICE_NAME, "dummy");
 	wait_signal (link_added);
+
 	g_assert (nm_platform_link_exists (NM_PLATFORM_GET, DEVICE_NAME));
 	ifindex = nm_platform_link_get_ifindex (NM_PLATFORM_GET, DEVICE_NAME);
 	g_assert (ifindex > 0);
@@ -496,7 +497,7 @@ test_external (void)
 
 	success = nm_platform_link_get (NM_PLATFORM_GET, ifindex, &link);
 	g_assert (success);
-	if (!link.driver) {
+	if (!link.initialized) {
 		/* we still lack the notification via UDEV. Expect another link changed signal. */
 		wait_signal (link_changed);
 	}
@@ -505,8 +506,10 @@ test_external (void)
 	g_assert (!nm_platform_link_is_up (NM_PLATFORM_GET, ifindex));
 	g_assert (!nm_platform_link_is_connected (NM_PLATFORM_GET, ifindex));
 	g_assert (!nm_platform_link_uses_arp (NM_PLATFORM_GET, ifindex));
+
 	run_command ("ip link set %s up", DEVICE_NAME);
 	wait_signal (link_changed);
+
 	g_assert (nm_platform_link_is_up (NM_PLATFORM_GET, ifindex));
 	g_assert (nm_platform_link_is_connected (NM_PLATFORM_GET, ifindex));
 	run_command ("ip link set %s down", DEVICE_NAME);

src/platform/tests/test-route.c
@@ -325,7 +325,7 @@ setup_tests (void)
 	nm_platform_link_delete (NM_PLATFORM_GET, nm_platform_link_get_ifindex (NM_PLATFORM_GET, DEVICE_NAME));
 	g_assert (!nm_platform_link_exists (NM_PLATFORM_GET, DEVICE_NAME));
 	g_assert (nm_platform_dummy_add (NM_PLATFORM_GET, DEVICE_NAME));
-	wait_signal (link_added);
+	accept_signal (link_added);
 	free_signal (link_added);
 
 	g_assert (nm_platform_link_set_up (NM_PLATFORM_GET, nm_platform_link_get_ifindex (NM_PLATFORM_GET, DEVICE_NAME)));

src/tests/test-route-manager.c
@@ -640,7 +640,7 @@ fixture_setup (test_fixture *fixture, gconstpointer user_data)
 	nm_platform_link_delete (NM_PLATFORM_GET, nm_platform_link_get_ifindex (NM_PLATFORM_GET, "nm-test-device0"));
 	g_assert (!nm_platform_link_exists (NM_PLATFORM_GET, "nm-test-device0"));
 	g_assert (nm_platform_dummy_add (NM_PLATFORM_GET, "nm-test-device0"));
-	wait_signal (link_added);
+	accept_signal (link_added);
 	free_signal (link_added);
 	fixture->ifindex0 = nm_platform_link_get_ifindex (NM_PLATFORM_GET, "nm-test-device0");
 	g_assert (nm_platform_link_set_up (NM_PLATFORM_GET, fixture->ifindex0));
@@ -652,7 +652,7 @@ fixture_setup (test_fixture *fixture, gconstpointer user_data)
 	nm_platform_link_delete (NM_PLATFORM_GET, nm_platform_link_get_ifindex (NM_PLATFORM_GET, "nm-test-device1"));
 	g_assert (!nm_platform_link_exists (NM_PLATFORM_GET, "nm-test-device1"));
 	g_assert (nm_platform_dummy_add (NM_PLATFORM_GET, "nm-test-device1"));
-	wait_signal (link_added);
+	accept_signal (link_added);
 	free_signal (link_added);
 	fixture->ifindex1 = nm_platform_link_get_ifindex (NM_PLATFORM_GET, "nm-test-device1");
 	g_assert (nm_platform_link_set_up (NM_PLATFORM_GET, fixture->ifindex1));