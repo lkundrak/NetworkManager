lkundrak/NetworkManager

commit 71bde20c302ba321688f203a8c5cd1e2d296f0d1

core: let device plugins advertise supported link and setting types
Instead of looping over all plugins and asking each plugin whether it
can handle a link or a connection, have them advertise the link and
connection types they support, and use that when creating new devices.

src/devices/nm-device-factory.c
@@ -19,8 +19,19 @@
  */
 
 #include "config.h"
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <errno.h>
+#include <string.h>
+
+#include <gmodule.h>
 
 #include "nm-device-factory.h"
+#include "nm-logging.h"
+#include "nm-platform.h"
+
+const NMLinkType _nm_device_factory_no_default_links[] = { NM_LINK_TYPE_NONE };
+const char *_nm_device_factory_no_default_settings[] = { NULL };
 
 enum {
 	DEVICE_ADDED,
@@ -28,21 +39,6 @@ enum {
 	LAST_SIGNAL
 };
 static guint signals[LAST_SIGNAL] = { 0 };
-
-static GSList *internal_types = NULL;
-
-void
-_nm_device_factory_internal_register_type (GType factory_type)
-{
-	g_return_if_fail (g_slist_find (internal_types, GUINT_TO_POINTER (factory_type)) == NULL);
-	internal_types = g_slist_prepend (internal_types, GUINT_TO_POINTER (factory_type));
-}
-
-const GSList *
-nm_device_factory_get_internal_factory_types (void)
-{
-	return internal_types;
-}
 
 gboolean
 nm_device_factory_emit_component_added (NMDeviceFactory *factory, GObject *component)
@@ -108,12 +104,24 @@ nm_device_factory_get_type (void)
 	return device_factory_type;
 }
 
-NMDeviceType
-nm_device_factory_get_device_type (NMDeviceFactory *factory)
+void
+nm_device_factory_get_supported_types (NMDeviceFactory *factory,
+                                       const NMLinkType **out_link_types,
+                                       const char ***out_setting_types)
 {
-	g_return_val_if_fail (factory != NULL, NM_DEVICE_TYPE_UNKNOWN);
+	const NMLinkType *link_types_fallback;
+	const char **setting_types_fallback;
 
-	return NM_DEVICE_FACTORY_GET_INTERFACE (factory)->get_device_type (factory);
+	g_return_if_fail (factory != NULL);
+
+	if (!out_link_types)
+		out_link_types = &link_types_fallback;
+	if (!out_setting_types)
+		out_setting_types = &setting_types_fallback;
+
+	NM_DEVICE_FACTORY_GET_INTERFACE (factory)->get_supported_types (factory,
+	                                                                out_link_types,
+	                                                                out_setting_types);
 }
 
 void
@@ -130,12 +138,38 @@ nm_device_factory_new_link (NMDeviceFactory *factory,
                             NMPlatformLink *plink,
                             GError **error)
 {
+	NMDeviceFactory *interface;
+	const NMLinkType *link_types = NULL;
+	const char **setting_types = NULL;
+	int i;
+
 	g_return_val_if_fail (factory != NULL, NULL);
 	g_return_val_if_fail (plink != NULL, NULL);
 
-	if (NM_DEVICE_FACTORY_GET_INTERFACE (factory)->new_link)
-		return NM_DEVICE_FACTORY_GET_INTERFACE (factory)->new_link (factory, plink, error);
-	return NULL;
+	/* Ensure the factory can create interfaces for this connection */
+	nm_device_factory_get_supported_types (factory, &link_types, &setting_types);
+	for (i = 0; link_types[i] > NM_LINK_TYPE_UNKNOWN; i++) {
+		if (plink->type == link_types[i])
+			break;
+	}
+
+	if (link_types[i] == NM_LINK_TYPE_UNKNOWN) {
+		g_set_error (error, NM_DEVICE_ERROR, NM_DEVICE_ERROR_CREATION_FAILED,
+		             "Device factory %s does not support link type %s (%d)",
+		             G_OBJECT_TYPE_NAME (factory),
+		             plink->kind, plink->type);
+		return NULL;
+	}
+
+	interface = NM_DEVICE_FACTORY_GET_INTERFACE (factory);
+	if (!interface->new_link) {
+		g_set_error (error, NM_MANAGER_ERROR, NM_MANAGER_ERROR_FAILED,
+		             "Device factory %s cannot manage new devices",
+		             G_OBJECT_TYPE_NAME (factory));
+		return NULL;
+	}
+
+	return interface->new_link (factory, plink, error);
 }
 
 NMDevice *
@@ -145,14 +179,334 @@ nm_device_factory_create_virtual_device_for_connection (NMDeviceFactory *factory
                                                         GError **error)
 {
 	NMDeviceFactory *interface;
+	const char **setting_types = NULL;
+	gboolean found = FALSE;
+	int i;
 
 	g_return_val_if_fail (factory, NULL);
 	g_return_val_if_fail (connection, NULL);
 	g_return_val_if_fail (!error || !*error, NULL);
 
+	/* Ensure the factory can create interfaces for this connection */
+	nm_device_factory_get_supported_types (factory, NULL, &setting_types);
+	for (i = 0; setting_types && setting_types[i]; i++) {
+		if (nm_connection_is_type (connection, setting_types[i])) {
+			found = TRUE;
+			break;
+		}
+	}
+
+	if (!found) {
+		g_set_error (error, NM_DEVICE_ERROR, NM_DEVICE_ERROR_INCOMPATIBLE_CONNECTION,
+		             "Device factory %s does not support connection type %s",
+		             G_OBJECT_TYPE_NAME (factory),
+		             nm_connection_get_connection_type (connection));
+		return NULL;
+	}
+
 	interface = NM_DEVICE_FACTORY_GET_INTERFACE (factory);
-	if (interface->create_virtual_device_for_connection)
-		return interface->create_virtual_device_for_connection (factory, connection, parent, error);
+	if (!interface->create_virtual_device_for_connection) {
+		g_set_error (error, NM_MANAGER_ERROR, NM_MANAGER_ERROR_FAILED,
+		             "Device factory %s cannot create virtual devices",
+		             G_OBJECT_TYPE_NAME (factory));
+		return NULL;
+	}
+
+	return interface->create_virtual_device_for_connection (factory, connection, parent, error);
+}
+
+/*******************************************************************/
+
+static GSList *internal_types = NULL;
+static GHashTable *factories_by_link = NULL;
+static GHashTable *factories_by_setting = NULL;
+
+void
+_nm_device_factory_internal_register_type (GType factory_type)
+{
+	g_return_if_fail (g_slist_find (internal_types, GUINT_TO_POINTER (factory_type)) == NULL);
+	internal_types = g_slist_prepend (internal_types, GUINT_TO_POINTER (factory_type));
+}
+
+static void __attribute__((destructor))
+_cleanup (void)
+{
+	g_clear_pointer (&internal_types, g_slist_free);
+	g_clear_pointer (&factories_by_link, g_hash_table_unref);
+	g_clear_pointer (&factories_by_setting, g_hash_table_unref);
+}
+
+static NMDeviceFactory *
+find_factory (const NMLinkType *needle_link_types,
+              const char **needle_setting_types)
+{
+	NMDeviceFactory *found;
+	guint i;
+
+	g_return_val_if_fail (factories_by_link, NULL);
+	g_return_val_if_fail (factories_by_setting, NULL);
+
+	/* NMLinkType search */
+	for (i = 0; needle_link_types && needle_link_types[i] > NM_LINK_TYPE_UNKNOWN; i++) {
+		found = g_hash_table_lookup (factories_by_link, GUINT_TO_POINTER (needle_link_types[i]));
+		if (found)
+			return found;
+	}
+
+	/* NMSetting name search */
+	for (i = 0; needle_setting_types && needle_setting_types[i]; i++) {
+		found = g_hash_table_lookup (factories_by_setting, needle_setting_types[i]);
+		if (found)
+			return found;
+	}
+
 	return NULL;
+}
+
+NMDeviceFactory *
+nm_device_factory_manager_find_factory_for_link_type (NMLinkType link_type)
+{
+	const NMLinkType ltypes[2] = { link_type, NM_LINK_TYPE_NONE };
+
+	g_assert (ltypes[0] > NM_LINK_TYPE_UNKNOWN);
+	return find_factory (ltypes, NULL);
+}
+
+NMDeviceFactory *
+nm_device_factory_manager_find_factory_for_connection (NMConnection *connection)
+{
+	const char *stypes[2] = { nm_connection_get_connection_type (connection), NULL };
+
+	g_assert (stypes[0]);
+	return find_factory (NULL, stypes);
+}
+
+void
+nm_device_factory_manager_for_each_factory (NMDeviceFactoryManagerFactoryFunc callback,
+                                            gpointer user_data)
+{
+	GHashTableIter iter;
+	NMDeviceFactory *factory;
+	GSList *list_iter, *list = NULL;
+
+	g_return_if_fail (factories_by_link);
+	g_return_if_fail (factories_by_setting);
+
+	g_hash_table_iter_init (&iter, factories_by_link);
+	while (g_hash_table_iter_next (&iter, NULL, (gpointer) &factory)) {
+		if (!g_slist_find (list, factory))
+			list = g_slist_prepend (list, factory);
+	}
+
+	g_hash_table_iter_init (&iter, factories_by_setting);
+	while (g_hash_table_iter_next (&iter, NULL, (gpointer) &factory)) {
+		if (!g_slist_find (list, factory))
+			list = g_slist_prepend (list, factory);
+	}
+
+	for (list_iter = list; list_iter; list_iter = list_iter->next)
+		callback (list_iter->data, user_data);
+
+	g_slist_free (list);
+}
+
+#define PLUGIN_PREFIX "libnm-device-plugin-"
+#define PLUGIN_PATH_TAG "NMManager-plugin-path"
+
+struct read_device_factory_paths_data {
+	char *path;
+	struct stat st;
+};
+
+static gint
+read_device_factory_paths_sort_fcn (gconstpointer a, gconstpointer b)
+{
+	const struct read_device_factory_paths_data *da = a;
+	const struct read_device_factory_paths_data *db = b;
+	time_t ta, tb;
+
+	ta = MAX (da->st.st_mtime, da->st.st_ctime);
+	tb = MAX (db->st.st_mtime, db->st.st_ctime);
+
+	if (ta < tb)
+		return 1;
+	if (ta > tb)
+		return -1;
+	return 0;
+}
+
+static char**
+read_device_factory_paths (void)
+{
+	GDir *dir;
+	GError *error = NULL;
+	const char *item;
+	GArray *paths;
+	char **result;
+	guint i;
+
+	dir = g_dir_open (NMPLUGINDIR, 0, &error);
+	if (!dir) {
+		nm_log_warn (LOGD_HW, "device plugin: failed to open directory %s: %s",
+		             NMPLUGINDIR,
+		             (error && error->message) ? error->message : "(unknown)");
+		g_clear_error (&error);
+		return NULL;
+	}
+
+	paths = g_array_new (FALSE, FALSE, sizeof (struct read_device_factory_paths_data));
+
+	while ((item = g_dir_read_name (dir))) {
+		int errsv;
+		struct read_device_factory_paths_data data;
+
+		if (!g_str_has_prefix (item, PLUGIN_PREFIX))
+			continue;
+		if (g_str_has_suffix (item, ".la"))
+			continue;
+
+		data.path = g_build_filename (NMPLUGINDIR, item, NULL);
+
+		if (stat (data.path, &data.st) != 0) {
+			errsv = errno;
+			nm_log_warn (LOGD_HW, "device plugin: skip invalid file %s (error during stat: %s)", data.path, strerror (errsv));
+			goto NEXT;
+		}
+		if (!S_ISREG (data.st.st_mode))
+			goto NEXT;
+		if (data.st.st_uid != 0) {
+			nm_log_warn (LOGD_HW, "device plugin: skip invalid file %s (file must be owned by root)", data.path);
+			goto NEXT;
+		}
+		if (data.st.st_mode & (S_IWGRP | S_IWOTH | S_ISUID)) {
+			nm_log_warn (LOGD_HW, "device plugin: skip invalid file %s (invalid file permissions)", data.path);
+			goto NEXT;
+		}
+
+		g_array_append_val (paths, data);
+		continue;
+NEXT:
+		g_free (data.path);
+	}
+	g_dir_close (dir);
+
+	/* sort filenames by modification time. */
+	g_array_sort (paths, read_device_factory_paths_sort_fcn);
+
+	result = g_new (char *, paths->len + 1);
+	for (i = 0; i < paths->len; i++)
+		result[i] = g_array_index (paths, struct read_device_factory_paths_data, i).path;
+	result[i] = NULL;
+
+	g_array_free (paths, TRUE);
+	return result;
+}
+
+static gboolean
+_add_factory (NMDeviceFactory *factory,
+              gboolean check_duplicates,
+              const char *path,
+              NMDeviceFactoryManagerFactoryFunc callback,
+              gpointer user_data)
+{
+	NMDeviceFactory *found = NULL;
+	const NMLinkType *link_types = NULL;
+	const char **setting_types = NULL;
+	int i;
+
+	g_return_val_if_fail (factories_by_link, FALSE);
+	g_return_val_if_fail (factories_by_setting, FALSE);
+
+	nm_device_factory_get_supported_types (factory, &link_types, &setting_types);
+	if (check_duplicates) {
+		found = find_factory (link_types, setting_types);
+		if (found) {
+			nm_log_warn (LOGD_HW, "Loading device plugin failed: multiple plugins "
+			             "for same type (using '%s' instead of '%s')",
+			             (char *) g_object_get_data (G_OBJECT (found), PLUGIN_PATH_TAG),
+			             path);
+			return FALSE;
+		}
+	}
+
+	g_object_set_data_full (G_OBJECT (factory), PLUGIN_PATH_TAG, g_strdup (path), g_free);
+	for (i = 0; link_types && link_types[i] > NM_LINK_TYPE_UNKNOWN; i++)
+		g_hash_table_insert (factories_by_link, GUINT_TO_POINTER (link_types[i]), g_object_ref (factory));
+	for (i = 0; setting_types && setting_types[i]; i++)
+		g_hash_table_insert (factories_by_setting, (char *) setting_types[i], g_object_ref (factory));
+
+	callback (factory, user_data);
+
+	nm_log_info (LOGD_HW, "Loaded device plugin: %s (%s)", G_OBJECT_TYPE_NAME (factory), path);
+	return TRUE;
+}
+
+void
+nm_device_factory_manager_load_factories (NMDeviceFactoryManagerFactoryFunc callback,
+                                          gpointer user_data)
+{
+	NMDeviceFactory *factory;
+	const GSList *iter;
+	GError *error = NULL;
+	char **path, **paths;
+
+	g_return_if_fail (factories_by_link == NULL);
+	g_return_if_fail (factories_by_setting == NULL);
+
+	factories_by_link = g_hash_table_new_full (g_direct_hash, g_direct_equal, NULL, g_object_unref);
+	factories_by_setting = g_hash_table_new_full (g_str_hash, g_str_equal, NULL, g_object_unref);
+
+	/* Register internal factories first */
+	for (iter = internal_types; iter; iter = iter->next) {
+		GType ftype = (GType) GPOINTER_TO_SIZE (iter->data);
+
+		factory = (NMDeviceFactory *) g_object_new (ftype, NULL);
+		g_assert (factory);
+		_add_factory (factory, FALSE, "internal", callback, user_data);
+	}
+
+	paths = read_device_factory_paths ();
+	if (!paths)
+		return;
+
+	for (path = paths; *path; path++) {
+		GModule *plugin;
+		NMDeviceFactoryCreateFunc create_func;
+		const char *item;
+
+		item = strrchr (*path, '/');
+		g_assert (item);
+
+		plugin = g_module_open (*path, G_MODULE_BIND_LOCAL);
+
+		if (!plugin) {
+			nm_log_warn (LOGD_HW, "(%s): failed to load plugin: %s", item, g_module_error ());
+			continue;
+		}
+
+		if (!g_module_symbol (plugin, "nm_device_factory_create", (gpointer) &create_func)) {
+			nm_log_warn (LOGD_HW, "(%s): failed to find device factory creator: %s", item, g_module_error ());
+			g_module_close (plugin);
+			continue;
+		}
+
+		factory = create_func (&error);
+		if (!factory) {
+			nm_log_warn (LOGD_HW, "(%s): failed to initialize device factory: %s",
+			             item, error ? error->message : "unknown");
+			g_clear_error (&error);
+			g_module_close (plugin);
+			continue;
+		}
+		g_clear_error (&error);
+
+		if (_add_factory (factory, TRUE, g_module_name (plugin), callback, user_data))
+			g_module_make_resident (plugin);
+		else
+			g_module_close (plugin);
+
+		g_object_unref (factory);
+	}
+	g_strfreev (paths);
 }
 

src/devices/adsl/nm-atm-manager.c
@@ -25,6 +25,7 @@
 #include <gmodule.h>
 
 #include "nm-atm-manager.h"
+#include "nm-setting-adsl.h"
 #include "nm-device-adsl.h"
 #include "nm-device-factory.h"
 #include "nm-logging.h"
@@ -203,11 +204,9 @@ handle_uevent (GUdevClient *client,
 		adsl_remove (self, device);
 }
 
-static NMDeviceType
-get_device_type (NMDeviceFactory *factory)
-{
-	return NM_DEVICE_TYPE_ADSL;
-}
+NM_DEVICE_FACTORY_DECLARE_TYPES (
+	NM_DEVICE_FACTORY_DECLARE_SETTING_TYPES (NM_SETTING_ADSL_SETTING_NAME)
+)
 
 /*********************************************************************/
 
@@ -224,7 +223,7 @@ nm_atm_manager_init (NMAtmManager *self)
 static void
 device_factory_interface_init (NMDeviceFactory *factory_iface)
 {
-	factory_iface->get_device_type = get_device_type;
+	factory_iface->get_supported_types = get_supported_types;
 	factory_iface->start = start;
 }
 

src/devices/bluetooth/nm-bluez-manager.c
@@ -29,6 +29,7 @@
 #include "nm-logging.h"
 #include "nm-bluez-manager.h"
 #include "nm-device-factory.h"
+#include "nm-setting-bluetooth.h"
 #include "nm-bluez4-manager.h"
 #include "nm-bluez5-manager.h"
 #include "nm-bluez-device.h"
@@ -367,11 +368,9 @@ start (NMDeviceFactory *factory)
 	check_bluez_and_try_setup (NM_BLUEZ_MANAGER (factory));
 }
 
-static NMDeviceType
-get_device_type (NMDeviceFactory *factory)
-{
-	return NM_DEVICE_TYPE_BT;
-}
+NM_DEVICE_FACTORY_DECLARE_TYPES (
+	NM_DEVICE_FACTORY_DECLARE_SETTING_TYPES (NM_SETTING_BLUETOOTH_SETTING_NAME)
+)
 
 /*********************************************************************/
 
@@ -407,7 +406,7 @@ nm_bluez_manager_init (NMBluezManager *self)
 static void
 device_factory_interface_init (NMDeviceFactory *factory_iface)
 {
-	factory_iface->get_device_type = get_device_type;
+	factory_iface->get_supported_types = get_supported_types;
 	factory_iface->start = start;
 }
 

src/devices/nm-device-bond.c
@@ -555,16 +555,13 @@ nm_device_bond_class_init (NMDeviceBondClass *klass)
 static NMDevice *
 new_link (NMDeviceFactory *factory, NMPlatformLink *plink, GError **error)
 {
-	if (plink->type == NM_LINK_TYPE_BOND) {
-		return (NMDevice *) g_object_new (NM_TYPE_DEVICE_BOND,
-		                                  NM_DEVICE_PLATFORM_DEVICE, plink,
-		                                  NM_DEVICE_DRIVER, "bonding",
-		                                  NM_DEVICE_TYPE_DESC, "Bond",
-		                                  NM_DEVICE_DEVICE_TYPE, NM_DEVICE_TYPE_BOND,
-		                                  NM_DEVICE_IS_MASTER, TRUE,
-		                                  NULL);
-	}
-	return NULL;
+	return (NMDevice *) g_object_new (NM_TYPE_DEVICE_BOND,
+	                                  NM_DEVICE_PLATFORM_DEVICE, plink,
+	                                  NM_DEVICE_DRIVER, "bonding",
+	                                  NM_DEVICE_TYPE_DESC, "Bond",
+	                                  NM_DEVICE_DEVICE_TYPE, NM_DEVICE_TYPE_BOND,
+	                                  NM_DEVICE_IS_MASTER, TRUE,
+	                                  NULL);
 }
 
 static NMDevice *
@@ -573,19 +570,17 @@ create_virtual_device_for_connection (NMDeviceFactory *factory,
                                       NMDevice *parent,
                                       GError **error)
 {
-	const char *iface;
+	const char *iface = nm_connection_get_interface_name (connection);
 
-	if (!nm_connection_is_type (connection, NM_SETTING_BOND_SETTING_NAME))
-		return NULL;
-
-	iface = nm_connection_get_interface_name (connection);
-	g_return_val_if_fail (iface != NULL, NULL);
+	g_assert (iface);
 
 	if (   !nm_platform_bond_add (NM_PLATFORM_GET, iface)
-		&& nm_platform_get_error (NM_PLATFORM_GET) != NM_PLATFORM_ERROR_EXISTS) {
-		nm_log_warn (LOGD_DEVICE | LOGD_BOND, "(%s): failed to create bonding master interface for '%s': %s",
-			         iface, nm_connection_get_id (connection),
-			         nm_platform_get_error_msg (NM_PLATFORM_GET));
+	    && nm_platform_get_error (NM_PLATFORM_GET) != NM_PLATFORM_ERROR_EXISTS) {
+		g_set_error (error, NM_DEVICE_ERROR, NM_DEVICE_ERROR_CREATION_FAILED,
+		             "Failed to create bond interface '%s' for '%s': %s",
+		             iface,
+		             nm_connection_get_id (connection),
+		             nm_platform_get_error_msg (NM_PLATFORM_GET));
 		return NULL;
 	}
 
@@ -598,7 +593,9 @@ create_virtual_device_for_connection (NMDeviceFactory *factory,
 		                              NULL);
 }
 
-DEFINE_DEVICE_FACTORY_INTERNAL(BOND, Bond, bond,
+NM_DEVICE_FACTORY_DEFINE_INTERNAL (BOND, Bond, bond,
+	NM_DEVICE_FACTORY_DECLARE_LINK_TYPES    (NM_LINK_TYPE_BOND)
+	NM_DEVICE_FACTORY_DECLARE_SETTING_TYPES (NM_SETTING_BOND_SETTING_NAME),
 	factory_iface->new_link = new_link;
 	factory_iface->create_virtual_device_for_connection = create_virtual_device_for_connection;
 	)

src/devices/nm-device-bridge.c
@@ -479,16 +479,13 @@ nm_device_bridge_class_init (NMDeviceBridgeClass *klass)
 static NMDevice *
 new_link (NMDeviceFactory *factory, NMPlatformLink *plink, GError **error)
 {
-	if (plink->type == NM_LINK_TYPE_BRIDGE) {
-		return (NMDevice *) g_object_new (NM_TYPE_DEVICE_BRIDGE,
-		                                  NM_DEVICE_PLATFORM_DEVICE, plink,
-		                                  NM_DEVICE_DRIVER, "bridge",
-		                                  NM_DEVICE_TYPE_DESC, "Bridge",
-		                                  NM_DEVICE_DEVICE_TYPE, NM_DEVICE_TYPE_BRIDGE,
-		                                  NM_DEVICE_IS_MASTER, TRUE,
-		                                  NULL);
-	}
-	return NULL;
+	return (NMDevice *) g_object_new (NM_TYPE_DEVICE_BRIDGE,
+	                                  NM_DEVICE_PLATFORM_DEVICE, plink,
+	                                  NM_DEVICE_DRIVER, "bridge",
+	                                  NM_DEVICE_TYPE_DESC, "Bridge",
+	                                  NM_DEVICE_DEVICE_TYPE, NM_DEVICE_TYPE_BRIDGE,
+	                                  NM_DEVICE_IS_MASTER, TRUE,
+	                                  NULL);
 }
 
 static NMDevice *
@@ -497,21 +494,15 @@ create_virtual_device_for_connection (NMDeviceFactory *factory,
                                       NMDevice *parent,
                                       GError **error)
 {
-	const char *iface;
+	const char *iface = nm_connection_get_interface_name (connection);
 	NMSettingBridge *s_bridge;
 	const char *mac_address_str;
 	guint8 mac_address[NM_UTILS_HWADDR_LEN_MAX];
 
-	if (!nm_connection_is_type (connection, NM_SETTING_BRIDGE_SETTING_NAME))
-		return NULL;
-
-	g_return_val_if_fail (connection != NULL, NULL);
-
-	iface = nm_connection_get_interface_name (connection);
-	g_return_val_if_fail (iface != NULL, NULL);
+	g_assert (iface);
 
 	s_bridge = nm_connection_get_setting_bridge (connection);
-	g_return_val_if_fail (s_bridge, NULL);
+	g_assert (s_bridge);
 
 	mac_address_str = nm_setting_bridge_get_mac_address (s_bridge);
 	if (mac_address_str) {
@@ -524,8 +515,10 @@ create_virtual_device_for_connection (NMDeviceFactory *factory,
 	                                mac_address_str ? mac_address : NULL,
 	                                mac_address_str ? ETH_ALEN : 0)
 	    && nm_platform_get_error (NM_PLATFORM_GET) != NM_PLATFORM_ERROR_EXISTS) {
-		nm_log_warn (LOGD_DEVICE | LOGD_BRIDGE, "(%s): failed to create bridge master interface for '%s': %s",
-		             iface, nm_connection_get_id (connection),
+		g_set_error (error, NM_DEVICE_ERROR, NM_DEVICE_ERROR_CREATION_FAILED,
+		             "Failed to create bridge interface '%s' for '%s': %s",
+		             iface,
+		             nm_connection_get_id (connection),
 		             nm_platform_get_error_msg (NM_PLATFORM_GET));
 		return NULL;
 	}
@@ -539,7 +532,9 @@ create_virtual_device_for_connection (NMDeviceFactory *factory,
 	                                  NULL);
 }
 
-DEFINE_DEVICE_FACTORY_INTERNAL(BRIDGE, Bridge, bridge,
+NM_DEVICE_FACTORY_DEFINE_INTERNAL (BRIDGE, Bridge, bridge,
+	NM_DEVICE_FACTORY_DECLARE_LINK_TYPES    (NM_LINK_TYPE_BRIDGE)
+	NM_DEVICE_FACTORY_DECLARE_SETTING_TYPES (NM_SETTING_BRIDGE_SETTING_NAME),
 	factory_iface->new_link = new_link;
 	factory_iface->create_virtual_device_for_connection = create_virtual_device_for_connection;
 	)

src/devices/nm-device-ethernet.c
@@ -1757,17 +1757,16 @@ nm_device_ethernet_class_init (NMDeviceEthernetClass *klass)
 static NMDevice *
 new_link (NMDeviceFactory *factory, NMPlatformLink *plink, GError **error)
 {
-	if (plink->type == NM_LINK_TYPE_ETHERNET) {
-		return (NMDevice *) g_object_new (NM_TYPE_DEVICE_ETHERNET,
-		                                  NM_DEVICE_PLATFORM_DEVICE, plink,
-		                                  NM_DEVICE_TYPE_DESC, "Ethernet",
-		                                  NM_DEVICE_DEVICE_TYPE, NM_DEVICE_TYPE_ETHERNET,
-		                                  NULL);
-	}
-	return NULL;
+	return (NMDevice *) g_object_new (NM_TYPE_DEVICE_ETHERNET,
+	                                  NM_DEVICE_PLATFORM_DEVICE, plink,
+	                                  NM_DEVICE_TYPE_DESC, "Ethernet",
+	                                  NM_DEVICE_DEVICE_TYPE, NM_DEVICE_TYPE_ETHERNET,
+	                                  NULL);
 }
 
-DEFINE_DEVICE_FACTORY_INTERNAL(ETHERNET, Ethernet, ethernet, \
-	factory_iface->new_link = new_link; \
+NM_DEVICE_FACTORY_DEFINE_INTERNAL (ETHERNET, Ethernet, ethernet,
+	NM_DEVICE_FACTORY_DECLARE_LINK_TYPES    (NM_LINK_TYPE_ETHERNET)
+	NM_DEVICE_FACTORY_DECLARE_SETTING_TYPES (NM_SETTING_WIRED_SETTING_NAME, NM_SETTING_PPPOE_SETTING_NAME),
+	factory_iface->new_link = new_link;
 	)
 

commit 8109f49187cccab2e24d12aa2f27ae1c7074762b

device: fix wrong assert in NMDeviceFactory
nm_device_factory_manager_find_factory_for_link_type()
easily can see a link-type NM_LINK_TYPE_UNKNOWN because
there are many link types that NetworkManager cannot detect.

Just return NULL early.

Fixes: 71bde20c302ba321688f203a8c5cd1e2d296f0d1

src/devices/nm-device-factory.c
@@ -302,7 +302,9 @@ nm_device_factory_manager_find_factory_for_link_type (NMLinkType link_type)
 {
 	const NMLinkType ltypes[2] = { link_type, NM_LINK_TYPE_NONE };
 
-	g_assert (ltypes[0] > NM_LINK_TYPE_UNKNOWN);
+	if (link_type == NM_LINK_TYPE_UNKNOWN)
+		return NULL;
+	g_return_val_if_fail (link_type > NM_LINK_TYPE_UNKNOWN, NULL);
 	return find_factory (ltypes, NULL);
 }
 