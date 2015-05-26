lkundrak/NetworkManager

commit 6463ce5dd9ffc1aae3ca9ddc4dac4e25fd815237

tests: use the TAP formatter
The test results in standard format are easily integrated into CI systems.

tools/run-test-valgrind.sh
@@ -9,7 +9,7 @@ if [ "$1" = "--launch-dbus" ]; then
     trap "kill $DBUS_SESSION_BUS_PID" EXIT
     shift
 fi
-TEST="$1"; shift
+TEST="$1"
 
 if [ "$NMTST_NO_VALGRIND" != "" ]; then
 	"$TEST"
@@ -27,7 +27,7 @@ $LIBTOOL --mode=execute "$VALGRIND" \
 	--gen-suppressions=all \
 	--suppressions="$SUPPRESSIONS" \
 	--log-file="$LOGFILE" \
-	"$TEST"
+	"$@"
 RESULT=$?
 
 if [ $RESULT -eq 0 -a "$(wc -c "$LOGFILE" | awk '{print$1}')" -ne 0 ]; then

callouts/tests/Makefile.am
@@ -29,6 +29,8 @@ test_dispatcher_envp_LDADD = \
 ###########################################
 
 @VALGRIND_RULES@
+AM_TESTS_FD_REDIRECT = --tap
+LOG_DRIVER = $(top_srcdir)/build-aux/tap-driver.sh
 TESTS = test-dispatcher-envp
 
 endif

configure.ac
@@ -15,6 +15,7 @@ AC_INIT([NetworkManager], [nm_version],
 AC_CONFIG_HEADERS([config.h])
 AC_CONFIG_MACRO_DIR([m4])
 AC_CONFIG_AUX_DIR([build-aux])
+AC_REQUIRE_AUX_FILE([tap-driver.sh])
 
 AM_INIT_AUTOMAKE(1.12 tar-ustar no-dist-gzip dist-bzip2 -Wno-portability)  dnl NB: Do not [quote] this parameter.
 AM_MAINTAINER_MODE([enable])

libnm-core/tests/Makefile.am
@@ -26,6 +26,8 @@ LDADD = \
 	$(GLIB_LIBS)
 
 @VALGRIND_RULES@
+AM_TESTS_FD_REDIRECT = --tap
+LOG_DRIVER = $(top_srcdir)/build-aux/tap-driver.sh
 TESTS = $(noinst_PROGRAMS)
 
 endif

libnm-glib/tests/Makefile.am
@@ -17,6 +17,8 @@ if WITH_VALGRIND
 else
 LOG_COMPILER = $(srcdir)/libnm-glib-test-launch.sh
 endif
+AM_TESTS_FD_REDIRECT = --tap
+LOG_DRIVER = $(top_srcdir)/build-aux/tap-driver.sh
 TESTS = test-nm-client test-remote-settings-client
 
 ####### NMClient and non-settings tests #######

libnm-util/tests/Makefile.am
@@ -11,6 +11,8 @@ AM_CPPFLAGS = \
 	-DTEST_CERT_DIR=\"$(top_srcdir)/libnm-core/tests/certs/\"
 
 @VALGRIND_RULES@
+AM_TESTS_FD_REDIRECT = --tap
+LOG_DRIVER = $(top_srcdir)/build-aux/tap-driver.sh
 TESTS = \
 	test-settings-defaults \
 	test-crypto \

libnm/tests/Makefile.am
@@ -22,6 +22,8 @@ if WITH_VALGRIND
 else
 LOG_COMPILER = $(srcdir)/libnm-test-launch.sh
 endif
+AM_TESTS_FD_REDIRECT = --tap
+LOG_DRIVER = $(top_srcdir)/build-aux/tap-driver.sh
 TESTS = test-nm-client test-remote-settings-client test-secret-agent
 
 test_nm_client_SOURCES = \

src/devices/wifi/tests/Makefile.am
@@ -24,5 +24,7 @@ test_wifi_ap_utils_SOURCES = \
 test_wifi_ap_utils_LDADD = $(top_builddir)/src/libNetworkManager.la
 
 @VALGRIND_RULES@
+AM_TESTS_FD_REDIRECT = --tap
+LOG_DRIVER = $(top_srcdir)/build-aux/tap-driver.sh
 TESTS = test-wifi-ap-utils
 

commit 05cebb21757c39f46e2e0578a959cb7dacc7aeb7

test: fix running valgrind tests with NMTST_NO_VALGRIND
Fixes: 6463ce5dd9ffc1aae3ca9ddc4dac4e25fd815237

tools/run-test-valgrind.sh
@@ -12,7 +12,7 @@ fi
 TEST="$1"
 
 if [ "$NMTST_NO_VALGRIND" != "" ]; then
-	"$TEST"
+	"$@"
 	exit $?
 fi
 