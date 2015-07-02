lkundrak/NetworkManager

commit dc0193ac023e30a9a3794e78c66f8c5266695a60

config: support a [connection] section to NetworkManager.conf to specify connection defaults
Add support for a new section [connection] in NetworkManager.conf.
If the connection leaves an option at "unknown"/"default", we can
support overwriting the value from global configuration.

We also support other sections that are named with "connection"
as a prefix, such as [connection2], [connection-wifi]. This is
to support multiple default values that can be applied depending
on the used device.

I think this has great potential. Only downside is that when
the user looks at a connection value, it will see that it is
unspecified. But the actually used value depends on the device
type and might not be obvious.

https://bugzilla.gnome.org/show_bug.cgi?id=695383
https://bugzilla.redhat.com/show_bug.cgi?id=1164677

man/NetworkManager.conf.xml.in
@@ -454,6 +454,77 @@ unmanaged-devices=mac:00:22:68:1c:59:b1;mac:00:1E:65:30:D1:C4;interface-name:eth
   </refsect1>
 
   <refsect1>
+    <title><literal>connection</literal> section</title>
+    <para>This section allows to specify default values for
+    connections. Not all properties can be overwritten, only a selected
+    list below. You can have multiple <literal>connection</literal>
+    sections, by having different sections with a name that all start
+    with "connection".</para>
+    <para>
+        Example:
+<programlisting>
+[connection]
+ipv6.ip6-privacy=0
+
+[connection-wifi-wlan0]
+match-device=interface-name:wlan0
+ipv4.route-metric=50
+
+[connection-wifi-other]
+match-device=type:wifi
+ipv4.route-metric=55
+ipv6.ip6-privacy=1
+</programlisting>
+    </para>
+
+    <para>
+        The sections are considered in order of appearance, with the
+        exception that the <literal>[connection]</literal> section is always
+        considered last. In the example above, this order is <literal>[connection-wifi-wlan0]</literal>,
+        <literal>[connection-wlan-other]</literal>, and <literal>[connection]</literal>.
+        When checking for a default configuration value, the section are searched until
+        the requested value is found.
+        In the example above, "ipv4.route-metric" for wlan0 interface is set to 50,
+        and for all other Wi-Fi typed interfaces to 55. Also, Wi-Fi devices would have
+        IPv6 private addresses enabled by default, but other devices would have it disabled.
+        Note that also "wlan0" gets "ipv6.ip6-privacy=1", because although the section
+        "[connection-wifi-wlan0]" matches the device, it does not contain that property
+        and the search continues.
+        This is just an example, currently these properties are not overwritable.
+    </para>
+
+    <para>
+      <variablelist>
+        <varlistentry>
+          <term><varname>match-device</varname></term>
+          <listitem><para>An optional device spec that restricts
+          when the section applies. See <xref linkend="device-spec"/>
+          for the possible values.
+          </para></listitem>
+        </varlistentry>
+        <varlistentry>
+          <term><varname>stop-match</varname></term>
+          <listitem><para>An optional boolean value which defaults to
+          <literal>no</literal>. If the section matches (based on
+          <literal>match-device</literal>), further sections will not be
+          considered even if the property in question is not present. In
+          the example above, if <literal>[connection-wifi-wlan0]</literal> would
+          have <literal>stop-match</literal> set to <literal>yes</literal>,
+          its <literal>ipv6.ip6-privacy</literal> value would be
+          unspecified.
+          </para></listitem>
+        </varlistentry>
+      </variablelist>
+    </para>
+
+    <para>
+      The following properties are supported to have their default values configured:
+      <variablelist>
+      </variablelist>
+    </para>
+  </refsect1>
+
+  <refsect1>
     <title><literal>connectivity</literal> section</title>
     <para>This section controls NetworkManager's optional connectivity
     checking functionality.  This allows NetworkManager to detect

src/nm-config-data.c
@@ -27,12 +27,29 @@
 #include "gsystem-local-alloc.h"
 #include "nm-device.h"
 #include "nm-core-internal.h"
+#include "nm-macros-internal.h"
+
+typedef struct {
+	char *group_name;
+	gboolean stop_match;
+	struct {
+		/* have a separate boolean field @has, because a @spec with
+		 * value %NULL does not necessarily mean, that the property
+		 * "match-device" was unspecified. */
+		gboolean has;
+		GSList *spec;
+	} match_device;
+} ConnectionInfo;
 
 typedef struct {
 	char *config_main_file;
 	char *config_description;
 
 	GKeyFile *keyfile;
+
+	/* A zero-terminated list of pre-processed information from the
+	 * [connection] sections. This is to speed up lookup. */
+	ConnectionInfo *connection_infos;
 
 	struct {
 		char *uri;
@@ -168,6 +185,100 @@ nm_config_data_get_assume_ipv6ll_only (const NMConfigData *self, NMDevice *devic
 	g_return_val_if_fail (NM_IS_DEVICE (device), FALSE);
 
 	return nm_device_spec_match_list (device, NM_CONFIG_DATA_GET_PRIVATE (self)->assume_ipv6ll_only);
+}
+
+/************************************************************************/
+
+char *
+nm_config_data_get_connection_default (const NMConfigData *self,
+                                       const char *property,
+                                       NMDevice *device)
+{
+	NMConfigDataPrivate *priv;
+	const ConnectionInfo *connection_info;
+
+	g_return_val_if_fail (self, NULL);
+	g_return_val_if_fail (property && *property, NULL);
+	g_return_val_if_fail (strchr (property, '.'), NULL);
+
+	priv = NM_CONFIG_DATA_GET_PRIVATE (self);
+
+	if (!priv->connection_infos)
+		return NULL;
+
+	for (connection_info = &priv->connection_infos[0]; connection_info->group_name; connection_info++) {
+		char *value;
+		gboolean match;
+
+		value = g_key_file_get_value (priv->keyfile, connection_info->group_name, property, NULL);
+		if (!value && !connection_info->stop_match)
+			continue;
+
+		match = TRUE;
+		if (connection_info->match_device.has)
+			match = device && nm_device_spec_match_list (device, connection_info->match_device.spec);
+
+		if (match)
+			return value;
+		g_free (value);
+	}
+	return NULL;
+}
+
+static ConnectionInfo *
+_get_connection_infos (GKeyFile *keyfile)
+{
+	char **groups;
+	guint i;
+	char *connection_tag = NULL;
+	GSList *connection_groups = NULL;
+	ConnectionInfo *connection_infos = NULL;
+
+	/* get the list of existing [connection.\+] sections that we consider
+	 * for nm_config_data_get_connection_default(). Also, get them
+	 * in the right order. */
+	groups = g_key_file_get_groups (keyfile, NULL);
+	for (i = 0; groups && groups[i]; i++) {
+		if (g_str_has_prefix (groups[i], "connection")) {
+			if (strlen (groups[i]) == STRLEN ("connection"))
+				connection_tag = groups[i];
+			else
+				connection_groups = g_slist_prepend (connection_groups, groups[i]);
+		} else
+			g_free (groups[i]);
+	}
+	g_free (groups);
+	if (connection_tag) {
+		/* We want the group "connection" checked at last, so that
+		 * all other "connection.\+" have preference. Those other
+		 * groups are checked in order of appearance. */
+		connection_groups = g_slist_prepend (connection_groups, connection_tag);
+	}
+	if (connection_groups) {
+		guint len = g_slist_length (connection_groups);
+		GSList *iter;
+
+		connection_infos = g_new0 (ConnectionInfo, len + 1);
+		for (iter = connection_groups; iter; iter = iter->next) {
+			ConnectionInfo *connection_info;
+			char *value;
+
+			nm_assert (len >= 1);
+			connection_info = &connection_infos[--len];
+			connection_info->group_name = iter->data;
+
+			value = g_key_file_get_value (keyfile, iter->data, "match-device", NULL);
+			if (value) {
+				connection_info->match_device.has = TRUE;
+				connection_info->match_device.spec = nm_match_spec_split (value);
+				g_free (value);
+			}
+			connection_info->stop_match = nm_config_keyfile_get_boolean (keyfile, iter->data, "stop-match", FALSE);
+		}
+		g_slist_free (connection_groups);
+	}
+
+	return connection_infos;
 }
 
 /************************************************************************/
@@ -324,6 +435,7 @@ static void
 finalize (GObject *gobject)
 {
 	NMConfigDataPrivate *priv = NM_CONFIG_DATA_GET_PRIVATE (gobject);
+	guint i;
 
 	g_free (priv->config_main_file);
 	g_free (priv->config_description);
@@ -339,6 +451,14 @@ finalize (GObject *gobject)
 
 	g_slist_free_full (priv->ignore_carrier, g_free);
 	g_slist_free_full (priv->assume_ipv6ll_only, g_free);
+
+	if (priv->connection_infos) {
+		for (i = 0; priv->connection_infos[i].group_name; i++) {
+			g_free (priv->connection_infos[i].group_name);
+			g_slist_free_full (priv->connection_infos[i].match_device.spec, g_free);
+		}
+		g_free (priv->connection_infos);
+	}
 
 	g_key_file_unref (priv->keyfile);
 
@@ -356,6 +476,8 @@ constructed (GObject *object)
 	NMConfigData *self = NM_CONFIG_DATA (object);
 	NMConfigDataPrivate *priv = NM_CONFIG_DATA_GET_PRIVATE (self);
 	char *interval;
+
+	priv->connection_infos = _get_connection_infos (priv->keyfile);
 
 	priv->connectivity.uri = g_key_file_get_value (priv->keyfile, "connectivity", "uri", NULL);
 	priv->connectivity.response = g_key_file_get_value (priv->keyfile, "connectivity", "response", NULL);

src/tests/config/NetworkManager.conf
@@ -13,3 +13,27 @@ response=Hello
 
 [extra-section]
 extra-key=some value
+
+
+
+[connection]
+ipv4.route-metric=50
+ipv6.ip6_privacy=0
+dummy.test1=no
+dummy.test2=no
+
+[connection.dev51]
+match-device=mac:00:00:00:00:00:51
+stop-match=yes
+ipv4.route-metric=51
+dummy.test1=yes
+
+[connection.dev52]
+match-device=mac:00:00:00:00:00:52
+ipv4.route-metric=52
+
+[connection.public]
+match-device=interface-name:wlan1
+# match-wifi is not yet implemented. Just an idea what could be useful.
+match-wifi=ssid:*[Ss]tarbucks*|*University*
+ipv6.ip6_privacy=2

src/tests/config/test-config.c
@@ -94,6 +94,9 @@ test_config_simple (void)
 	GError *error = NULL;
 	const char **plugins;
 	char *value;
+	gs_unref_object NMDevice *dev50 = nm_test_device_new ("00:00:00:00:00:50");
+	gs_unref_object NMDevice *dev51 = nm_test_device_new ("00:00:00:00:00:51");
+	gs_unref_object NMDevice *dev52 = nm_test_device_new ("00:00:00:00:00:52");
 
 	config = setup_config (NULL, SRCDIR "/NetworkManager.conf", "/no/such/dir", NULL);
 
@@ -121,6 +124,54 @@ test_config_simple (void)
 	g_assert (!value);
 	g_assert_error (error, G_KEY_FILE_ERROR, G_KEY_FILE_ERROR_GROUP_NOT_FOUND);
 	g_clear_error (&error);
+
+	value = nm_config_data_get_value (nm_config_get_data_orig (config), "connection", "ipv6.ip6_privacy", NULL);
+	g_assert_cmpstr (value, ==, "0");
+	g_free (value);
+
+	value = nm_config_data_get_value (nm_config_get_data_orig (config), "connection.dev51", "ipv4.route-metric", NULL);
+	g_assert_cmpstr (value, ==, "51");
+	g_free (value);
+
+
+	value = nm_config_data_get_connection_default (nm_config_get_data_orig (config), "ipv6.route-metric", NULL);
+	g_assert_cmpstr (value, ==, NULL);
+	g_free (value);
+
+
+	value = nm_config_data_get_connection_default (nm_config_get_data_orig (config), "ipv4.route-metric", NULL);
+	g_assert_cmpstr (value, ==, "50");
+	g_free (value);
+
+	value = nm_config_data_get_connection_default (nm_config_get_data_orig (config), "ipv4.route-metric", dev50);
+	g_assert_cmpstr (value, ==, "50");
+	g_free (value);
+
+	value = nm_config_data_get_connection_default (nm_config_get_data_orig (config), "ipv4.route-metric", dev51);
+	g_assert_cmpstr (value, ==, "51");
+	g_free (value);
+
+	value = nm_config_data_get_connection_default (nm_config_get_data_orig (config), "ipv4.route-metric", dev52);
+	g_assert_cmpstr (value, ==, "52");
+	g_free (value);
+
+
+	value = nm_config_data_get_connection_default (nm_config_get_data_orig (config), "dummy.test1", dev51);
+	g_assert_cmpstr (value, ==, "yes");
+	g_free (value);
+
+	value = nm_config_data_get_connection_default (nm_config_get_data_orig (config), "dummy.test1", dev50);
+	g_assert_cmpstr (value, ==, "no");
+	g_free (value);
+
+	value = nm_config_data_get_connection_default (nm_config_get_data_orig (config), "dummy.test2", dev51);
+	g_assert_cmpstr (value, ==, NULL);
+	g_free (value);
+
+	value = nm_config_data_get_connection_default (nm_config_get_data_orig (config), "dummy.test2", dev50);
+	g_assert_cmpstr (value, ==, "no");
+	g_free (value);
+
 
 	g_object_unref (config);
 }

src/nm-config-data.h
@@ -94,6 +94,10 @@ const char *nm_config_data_get_rc_manager (const NMConfigData *self);
 gboolean nm_config_data_get_ignore_carrier (const NMConfigData *self, NMDevice *device);
 gboolean nm_config_data_get_assume_ipv6ll_only (const NMConfigData *self, NMDevice *device);
 
+char *nm_config_data_get_connection_default (const NMConfigData *self,
+                                             const char *property,
+                                             NMDevice *device);
+
 G_END_DECLS
 
 #endif /* NM_CONFIG_DATA_H */

commit 8d4ffe08ea273c09269c2bb79e62163e4d037403

config: fix order of processing [connection] sections in NMConfig
We support the "NetworkManager.conf" sections '[connection]' and
'[connection.\+]' (with arbitrary suffix).

Fix the order of how we evaluate these section.

Note that the literal '[connection]' section is always evaluated lastly
after any other '[connection.\+]' section.

Within one file, we want to evaluate the sections in top-to-bottom
order. But accross multiple files, we want to order them
later-files-first. That gives a reasonable behavior if the user
looks at one file, and also if he wants to overwrite configuration
via configuration snippets like "conf.d/99-last.conf".

Note that if a later file extends/overwrites a section defined in an
earlier file, the section is still considered with lower priority
This is intentional, because the user ~extends~ a lower priority
section. If he wants to add a higher priority section, he should
choose a new suffix.

Fixes: dc0193ac023e30a9a3794e78c66f8c5266695a60

man/NetworkManager.conf.xml.in
@@ -482,7 +482,7 @@ ipv6.ip6-privacy=1
     </para>
 
     <para>
-        The sections are considered in order of appearance, with the
+        The sections within one file are considered in order of appearance, with the
         exception that the <literal>[connection]</literal> section is always
         considered last. In the example above, this order is <literal>[connection-wifi-wlan0]</literal>,
         <literal>[connection-wlan-other]</literal>, and <literal>[connection]</literal>.
@@ -494,6 +494,11 @@ ipv6.ip6-privacy=1
         Note that also "wlan0" gets "ipv6.ip6-privacy=1", because although the section
         "[connection-wifi-wlan0]" matches the device, it does not contain that property
         and the search continues.
+    </para>
+    <para>
+        When having different sections in multiple files, sections from files that are read
+        later have higher priority. So within one file the priority of the sections is
+        top-to-bottom. Across multiple files later definitions take precedence.
     </para>
 
     <para>

src/nm-config-data.c
@@ -225,58 +225,63 @@ nm_config_data_get_connection_default (const NMConfigData *self,
 	return NULL;
 }
 
+static void
+_get_connection_info_init (ConnectionInfo *connection_info, GKeyFile *keyfile, char *group)
+{
+	char *value;
+
+	/* pass ownership of @group on... */
+	connection_info->group_name = group;
+
+	value = g_key_file_get_value (keyfile, group, "match-device", NULL);
+	if (value) {
+		connection_info->match_device.has = TRUE;
+		connection_info->match_device.spec = nm_match_spec_split (value);
+		g_free (value);
+	}
+	connection_info->stop_match = nm_config_keyfile_get_boolean (keyfile, group, "stop-match", FALSE);
+}
+
 static ConnectionInfo *
 _get_connection_infos (GKeyFile *keyfile)
 {
 	char **groups;
-	guint i;
+	gsize i, j, ngroups;
 	char *connection_tag = NULL;
-	GSList *connection_groups = NULL;
 	ConnectionInfo *connection_infos = NULL;
 
 	/* get the list of existing [connection.\+] sections that we consider
-	 * for nm_config_data_get_connection_default(). Also, get them
-	 * in the right order. */
-	groups = g_key_file_get_groups (keyfile, NULL);
-	for (i = 0; groups && groups[i]; i++) {
-		if (g_str_has_prefix (groups[i], "connection")) {
-			if (strlen (groups[i]) == STRLEN ("connection"))
-				connection_tag = groups[i];
-			else
-				connection_groups = g_slist_prepend (connection_groups, groups[i]);
-		} else
-			g_free (groups[i]);
+	 * for nm_config_data_get_connection_default().
+	 *
+	 * We expect the sections in their right order, with lowest priority
+	 * first. Only exception is the (literal) [connection] section, which
+	 * we will always reorder to the end. */
+	groups = g_key_file_get_groups (keyfile, &ngroups);
+	if (!groups)
+		ngroups = 0;
+	else if (ngroups > 0) {
+		for (i = 0, j = 0; i < ngroups; i++) {
+			if (g_str_has_prefix (groups[i], "connection")) {
+				if (groups[i][STRLEN ("connection")] == '\0')
+					connection_tag = groups[i];
+				else
+					groups[j++] = groups[i];
+			} else
+				g_free (groups[i]);
+		}
+		ngroups = j;
+	}
+
+	connection_infos = g_new0 (ConnectionInfo, ngroups + 1 + (connection_tag ? 1 : 0));
+	for (i = 0; i < ngroups; i++) {
+		/* pass ownership of @group on... */
+		_get_connection_info_init (&connection_infos[i], keyfile, groups[ngroups - i - 1]);
+	}
+	if (connection_tag) {
+		/* pass ownership of @connection_tag on... */
+		_get_connection_info_init (&connection_infos[i], keyfile, connection_tag);
 	}
 	g_free (groups);
-	if (connection_tag) {
-		/* We want the group "connection" checked at last, so that
-		 * all other "connection.\+" have preference. Those other
-		 * groups are checked in order of appearance. */
-		connection_groups = g_slist_prepend (connection_groups, connection_tag);
-	}
-	if (connection_groups) {
-		guint len = g_slist_length (connection_groups);
-		GSList *iter;
-
-		connection_infos = g_new0 (ConnectionInfo, len + 1);
-		for (iter = connection_groups; iter; iter = iter->next) {
-			ConnectionInfo *connection_info;
-			char *value;
-
-			nm_assert (len >= 1);
-			connection_info = &connection_infos[--len];
-			connection_info->group_name = iter->data;
-
-			value = g_key_file_get_value (keyfile, iter->data, "match-device", NULL);
-			if (value) {
-				connection_info->match_device.has = TRUE;
-				connection_info->match_device.spec = nm_match_spec_split (value);
-				g_free (value);
-			}
-			connection_info->stop_match = nm_config_keyfile_get_boolean (keyfile, iter->data, "stop-match", FALSE);
-		}
-		g_slist_free (connection_groups);
-	}
 
 	return connection_infos;
 }

src/nm-config.c
@@ -408,6 +408,45 @@ nm_config_create_keyfile ()
 	return keyfile;
 }
 
+static int
+_sort_groups_cmp (const char **pa, const char **pb, gpointer dummy)
+{
+	const char *a, *b;
+	gboolean a_is_connection, b_is_connection;
+
+	/* basic NULL checking... */
+	if (pa == pb)
+		return 0;
+	if (!pa)
+		return -1;
+	if (!pb)
+		return 1;
+
+	a = *pa;
+	b = *pb;
+
+	a_is_connection = g_str_has_prefix (a, "connection");
+	b_is_connection = g_str_has_prefix (b, "connection");
+
+	if (a_is_connection != b_is_connection) {
+		/* one is a [connection*] entry, the other not. We sort [connection*] entires
+		 * after.  */
+		if (a_is_connection)
+			return 1;
+		return -1;
+	}
+	if (!a_is_connection) {
+		/* both are non-connection entries. Don't reorder. */
+		return 0;
+	}
+
+	/* both are [connection.\+] entires. Reverse their order.
+	 * One of the sections might be literally [connection]. That section
+	 * is special and it's order will be fixed later. It doesn't actually
+	 * matter here how it compares with [connection.\+] sections. */
+	return pa > pb ? -1 : 1;
+}
+
 static gboolean
 read_config (GKeyFile *keyfile, const char *path, GError **error)
 {
@@ -435,6 +474,25 @@ read_config (GKeyFile *keyfile, const char *path, GError **error)
 
 	/* Override the current settings with the new ones */
 	groups = g_key_file_get_groups (kf, &ngroups);
+	if (!groups)
+		ngroups = 0;
+
+	/* Within one file we reverse the order of the '[connection.\+] sections.
+	 * Here we merge the current file (@kf) into @keyfile. As we merge multiple
+	 * files, earlier sections (with lower priority) will be added first.
+	 * But within one file, we want a top-to-bottom order. This means we
+	 * must reverse the order within each file.
+	 * At the very end, we will revert the order of all sections again and
+	 * get thus the right behavior. This final reversing is done in
+	 * NMConfigData:_get_connection_infos().  */
+	if (ngroups > 1) {
+		g_qsort_with_data (groups,
+		                   ngroups,
+		                   sizeof (char *),
+		                   (GCompareDataFunc) _sort_groups_cmp,
+		                   NULL);
+	}
+
 	for (g = 0; groups[g]; g++) {
 		keys = g_key_file_get_keys (kf, groups[g], &nkeys, NULL);
 		if (!keys)

src/tests/config/NetworkManager.conf
@@ -22,6 +22,17 @@ ipv6.ip6_privacy=0
 dummy.test1=no
 dummy.test2=no
 
+ord.key00=A-0.0.00
+ord.key01=A-0.0.01
+ord.key02=A-0.0.02
+ord.key03=A-0.0.03
+ord.key04=A-0.0.04
+ord.key05=A-0.0.05
+ord.key06=A-0.0.06
+ord.key07=A-0.0.07
+ord.key08=A-0.0.08
+ord.key09=A-0.0.09
+
 [connection.dev51]
 match-device=mac:00:00:00:00:00:51
 stop-match=yes
@@ -37,3 +48,36 @@ match-device=interface-name:wlan1
 # match-wifi is not yet implemented. Just an idea what could be useful.
 match-wifi=ssid:*[Ss]tarbucks*|*University*
 ipv6.ip6_privacy=2
+
+
+# the following sections are tested for their order across
+# multiple files.
+[connection.ord.0.1]
+ord.key03=A-0.1.03
+ord.key04=A-0.1.04
+ord.key05=A-0.1.05
+ord.key06=A-0.1.06
+ord.key07=A-0.1.07
+ord.key08=A-0.1.08
+ord.key09=A-0.1.09
+ord.ovw01=A-0.1.ovw01
+[connection.ord.0.2]
+ord.key02=A-0.2.02
+ord.key03=A-0.2.03
+ord.key04=A-0.2.04
+ord.key05=A-0.2.05
+ord.key06=A-0.2.06
+ord.key07=A-0.2.07
+ord.key08=A-0.2.08
+ord.key09=A-0.2.09
+[connection.ord.0.3]
+ord.key01=A-0.3.01
+ord.key02=A-0.3.02
+ord.key03=A-0.3.03
+ord.key04=A-0.3.04
+ord.key05=A-0.3.05
+ord.key06=A-0.3.06
+ord.key07=A-0.3.07
+ord.key08=A-0.3.08
+ord.key09=A-0.3.09
+ord.ovw01=A-0.3.ovw01

src/tests/config/conf.d/00-overrides.conf
@@ -9,3 +9,25 @@ a=0
 b=0
 c=0
 
+
+# the following sections are tested for their order across
+# multiple files.
+[connection.ord.1.1]
+ord.key06=B-1.1.06
+ord.key07=B-1.1.07
+ord.key08=B-1.1.08
+ord.key09=B-1.1.09
+[connection.ord.1.2]
+ord.key05=B-1.2.05
+ord.key06=B-1.2.06
+ord.key07=B-1.2.07
+ord.key08=B-1.2.08
+ord.key09=B-1.2.09
+[connection.ord.1.3]
+ord.key04=B-1.3.04
+ord.key05=B-1.3.05
+ord.key06=B-1.3.06
+ord.key07=B-1.3.07
+ord.key08=B-1.3.08
+ord.key09=B-1.3.09
+

src/tests/config/conf.d/10-more.conf
@@ -9,3 +9,21 @@ uri=http://example.net
 a=10
 b=10
 
+# the following sections are tested for their order across
+# multiple files.
+[connection.ord.2.1]
+ord.key09=C-2.1.09
+[connection.ord.2.2]
+ord.key08=C-2.2.08
+ord.key09=C-2.2.09
+[connection.ord.2.3]
+ord.key07=C-2.3.07
+ord.key08=C-2.3.08
+ord.key09=C-2.3.09
+
+# you can overwrite individual settings in a file loaded
+# previously. But note that this does not bump the priority
+# of the section, i.e. [connection.ord.0.1] still has a pretty
+# low priority and is shadowed by [connection.ord.2.1].
+[connection.ord.0.1]
+ord.ovw01=C-0.1.ovw01

src/tests/config/test-config.c
@@ -324,6 +324,23 @@ test_config_confdir (void)
 	g_assert_cmpstr (value, ==, "0");
 	g_free (value);
 
+#define ASSERT_GET_CONN_DEFAULT(xconfig, xname, xvalue) \
+	G_STMT_START { \
+		gs_free char *_value = nm_config_data_get_connection_default (nm_config_get_data_orig (xconfig), (xname), NULL); \
+		g_assert_cmpstr (_value, ==, (xvalue)); \
+	} G_STMT_END
+	ASSERT_GET_CONN_DEFAULT (config, "ord.key00", "A-0.0.00");
+	ASSERT_GET_CONN_DEFAULT (config, "ord.key01", "A-0.3.01");
+	ASSERT_GET_CONN_DEFAULT (config, "ord.key02", "A-0.2.02");
+	ASSERT_GET_CONN_DEFAULT (config, "ord.key03", "A-0.1.03");
+	ASSERT_GET_CONN_DEFAULT (config, "ord.key04", "B-1.3.04");
+	ASSERT_GET_CONN_DEFAULT (config, "ord.key05", "B-1.2.05");
+	ASSERT_GET_CONN_DEFAULT (config, "ord.key06", "B-1.1.06");
+	ASSERT_GET_CONN_DEFAULT (config, "ord.key07", "C-2.3.07");
+	ASSERT_GET_CONN_DEFAULT (config, "ord.key08", "C-2.2.08");
+	ASSERT_GET_CONN_DEFAULT (config, "ord.key09", "C-2.1.09");
+	ASSERT_GET_CONN_DEFAULT (config, "ord.ovw01", "C-0.1.ovw01");
+
 	g_object_unref (config);
 }
 