lkundrak/NetworkManager

commit d51975ed921a5876b76e081b8f3df4e2ca1f1ca9

default-route: also configure default-routes for assumed connections
Previously for assumed connections we would never configure a default route.
That has serious problems for example in the following two scenarios:

  - the default-route might have a limited lifetime from a previous
    SLAAC/accept_ra setting. In this case, once we assume the connection
    we must also ensure that we extend the lifetime of the default
    route.
  - the gateway could be received via DHCP/RA and it might change.
    If we ignore default-routes for assumed connection we miss that
    change.

The problem is that the notion of "assumed connection" wrongly combines
two conflicting goals (related bug bgo#746440):
  a) have an external device that is entirely unmanged by NM.
  b) do a seamless takeover of a previously managed connection at start,
     but still fully manage.

This patch changes the handling of default-routes towards meaning b).

https://bugzilla.redhat.com/show_bug.cgi?id=1224291

src/devices/nm-device.c
@@ -3093,7 +3093,7 @@ ip4_config_merge_and_apply (NMDevice *self,
 	if (priv->wwan_ip4_config)
 		nm_ip4_config_merge (composite, priv->wwan_ip4_config);
 
-	/* Merge user overrides into the composite config. For assumed connection,
+	/* Merge user overrides into the composite config. For assumed connections,
 	 * con_ip4_config is empty. */
 	if (priv->con_ip4_config)
 		nm_ip4_config_merge (composite, priv->con_ip4_config);
@@ -3122,20 +3122,23 @@ ip4_config_merge_and_apply (NMDevice *self,
 		goto END_ADD_DEFAULT_ROUTE;
 	}
 
-	if (nm_device_uses_assumed_connection (self))
-		goto END_ADD_DEFAULT_ROUTE;
-
 	connection_has_default_route
 	    = nm_default_route_manager_ip4_connection_has_default_route (nm_default_route_manager_get (),
 	                                                                 connection, &connection_is_never_default);
 
 	if (   !priv->default_route.v4_configure_first_time
+	    && !nm_device_uses_assumed_connection (self)
 	    && connection_is_never_default) {
 		/* If the connection is explicitly configured as never-default, we enforce the (absense of the)
 		 * default-route only once. That allows the user to configure a connection as never-default,
 		 * but he can add default routes externally (via a dispatcher script) and NM will not interfere. */
 		goto END_ADD_DEFAULT_ROUTE;
 	}
+
+	/* At this point, we treat assumed and non-assumed connections alike.
+	 * For assumed connections we do that because we still manage RA and DHCP
+	 * leases for them, so we must extend/update the default route on commits.
+	 */
 
 	/* we are about to commit (for a non-assumed connection). Enforce whatever we have
 	 * configured. */
@@ -3716,20 +3719,23 @@ ip6_config_merge_and_apply (NMDevice *self,
 		goto END_ADD_DEFAULT_ROUTE;
 	}
 
-	if (nm_device_uses_assumed_connection (self))
-		goto END_ADD_DEFAULT_ROUTE;
-
 	connection_has_default_route
 	    = nm_default_route_manager_ip6_connection_has_default_route (nm_default_route_manager_get (),
 	                                                                 connection, &connection_is_never_default);
 
 	if (   !priv->default_route.v6_configure_first_time
+	    && !nm_device_uses_assumed_connection (self)
 	    && connection_is_never_default) {
 		/* If the connection is explicitly configured as never-default, we enforce the (absence of the)
 		 * default-route only once. That allows the user to configure a connection as never-default,
 		 * but he can add default routes externally (via a dispatcher script) and NM will not interfere. */
 		goto END_ADD_DEFAULT_ROUTE;
 	}
+
+	/* At this point, we treat assumed and non-assumed connections alike.
+	 * For assumed connections we do that because we still manage RA and DHCP
+	 * leases for them, so we must extend/update the default route on commits.
+	 */
 
 	/* we are about to commit (for a non-assumed connection). Enforce whatever we have
 	 * configured. */

commit 001e0f636b0fea2d948587c66babe375d843a77d

device: always assume default-route for generate-assumed-connections
Commit d51975e changed, that we treat assumed and non-assumed
connections the same with respect to the default route. This is
certainly wrong, if we have an nm-generated-assumed connection
at hand. In this case, NM just generated a connection based on what
was configured on the system. Looking at that result and re-enforcing
the default-route is wrong.

We want to manage the default-route for assumed, persistent connections.
If the connection was assumed and generated, we do not.

This commit reverts d51975ed for nm-generated-assumed connection and
restores the previous behavior.

https://bugzilla.redhat.com/show_bug.cgi?id=1244483

Fixes: d51975ed921a5876b76e081b8f3df4e2ca1f1ca9

src/devices/nm-device.c
@@ -3321,16 +3321,8 @@ ip4_config_merge_and_apply (NMDevice *self,
 		goto END_ADD_DEFAULT_ROUTE;
 	}
 
-	connection_has_default_route
-	    = nm_default_route_manager_ip4_connection_has_default_route (nm_default_route_manager_get (),
-	                                                                 connection, &connection_is_never_default);
-
-	if (   !priv->v4_commit_first_time
-	    && !nm_device_uses_assumed_connection (self)
-	    && connection_is_never_default) {
-		/* If the connection is explicitly configured as never-default, we enforce the (absence of the)
-		 * default-route only once. That allows the user to configure a connection as never-default,
-		 * but he can add default routes externally (via a dispatcher script) and NM will not interfere. */
+	if (nm_device_uses_generated_assumed_connection (self)) {
+		/* a generate-assumed-connection always detects the default route from platform */
 		goto END_ADD_DEFAULT_ROUTE;
 	}
 
@@ -3338,6 +3330,18 @@ ip4_config_merge_and_apply (NMDevice *self,
 	 * For assumed connections we do that because we still manage RA and DHCP
 	 * leases for them, so we must extend/update the default route on commits.
 	 */
+
+	connection_has_default_route
+	    = nm_default_route_manager_ip4_connection_has_default_route (nm_default_route_manager_get (),
+	                                                                 connection, &connection_is_never_default);
+
+	if (   !priv->v4_commit_first_time
+	    && connection_is_never_default) {
+		/* If the connection is explicitly configured as never-default, we enforce the (absence of the)
+		 * default-route only once. That allows the user to configure a connection as never-default,
+		 * but he can add default routes externally (via a dispatcher script) and NM will not interfere. */
+		goto END_ADD_DEFAULT_ROUTE;
+	}
 
 	/* we are about to commit (for a non-assumed connection). Enforce whatever we have
 	 * configured. */
@@ -3942,16 +3946,8 @@ ip6_config_merge_and_apply (NMDevice *self,
 		goto END_ADD_DEFAULT_ROUTE;
 	}
 
-	connection_has_default_route
-	    = nm_default_route_manager_ip6_connection_has_default_route (nm_default_route_manager_get (),
-	                                                                 connection, &connection_is_never_default);
-
-	if (   !priv->v6_commit_first_time
-	    && !nm_device_uses_assumed_connection (self)
-	    && connection_is_never_default) {
-		/* If the connection is explicitly configured as never-default, we enforce the (absence of the)
-		 * default-route only once. That allows the user to configure a connection as never-default,
-		 * but he can add default routes externally (via a dispatcher script) and NM will not interfere. */
+	if (nm_device_uses_generated_assumed_connection (self)) {
+		/* a generate-assumed-connection always detects the default route from platform */
 		goto END_ADD_DEFAULT_ROUTE;
 	}
 
@@ -3959,6 +3955,18 @@ ip6_config_merge_and_apply (NMDevice *self,
 	 * For assumed connections we do that because we still manage RA and DHCP
 	 * leases for them, so we must extend/update the default route on commits.
 	 */
+
+	connection_has_default_route
+	    = nm_default_route_manager_ip6_connection_has_default_route (nm_default_route_manager_get (),
+	                                                                 connection, &connection_is_never_default);
+
+	if (   !priv->v6_commit_first_time
+	    && connection_is_never_default) {
+		/* If the connection is explicitly configured as never-default, we enforce the (absence of the)
+		 * default-route only once. That allows the user to configure a connection as never-default,
+		 * but he can add default routes externally (via a dispatcher script) and NM will not interfere. */
+		goto END_ADD_DEFAULT_ROUTE;
+	}
 
 	/* we are about to commit (for a non-assumed connection). Enforce whatever we have
 	 * configured. */