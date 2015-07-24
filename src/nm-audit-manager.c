e942a66f0ed3c829e5b9cc72e77044fe879239d3
Thomas Haller <thaller@redhat.com>
2015-07-24 12:48:54 +0200

    fixup! core: add audit support
    Close auditd fd on dispose()

--- src/nm-audit-manager.c
+++ src/nm-audit-manager.c
@@ -352,6 +352,11 @@ dispose (GObject *object)
 		g_signal_handlers_disconnect_by_func (priv->config, config_changed_cb, self);
 		g_clear_object (&priv->config);
 	}
+
+	 if (priv->auditd_fd >= 0) {
+		audit_close (priv->auditd_fd);
+		priv->auditd_fd = -1;
+	}
 #endif
 
 	G_OBJECT_CLASS (nm_audit_manager_parent_class)->dispose (object);
