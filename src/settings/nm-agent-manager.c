lkundrak/NetworkManager

commit 13386f760ac46564c363ea690d517d941fd1686e

agent-manager: fix leak of secret-agent

src/settings/nm-agent-manager.c
@@ -500,6 +500,9 @@ request_free (Request *req)
 
 	g_object_unref (req->self);
 
+	if (req->current)
+		g_object_unref (req->current);
+
 	memset (req, 0, sizeof (Request));
 	g_free (req);
 }
@@ -656,15 +659,19 @@ request_remove_agent (Request *req, NMSecretAgent *agent, GSList **pending_reqs)
 
 	self = req->self;
 
-	req->pending = g_slist_remove (req->pending, agent);
-
 	if (agent == req->current) {
+		nm_assert (!g_slist_find (req->pending, agent));
+
 		_LOGD (agent, "current agent removed from secrets request "LOG_REQ_FMT,
 		       LOG_REQ_ARG (req));
 		*pending_reqs = g_slist_prepend (*pending_reqs, req);
 	} else {
+		req->pending = g_slist_remove (req->pending, agent);
+
 		_LOGD (agent, "agent removed from secrets request "LOG_REQ_FMT,
 		       LOG_REQ_ARG (req));
+
+		g_object_unref (agent);
 	}
 }
 

commit 0af5f19e303a0532bd72357cbca751f12171738a

agent-manager: fix wrong unref in request_remove_agent()
Fixes: 13386f760ac46564c363ea690d517d941fd1686e

src/settings/nm-agent-manager.c
@@ -665,7 +665,7 @@ request_remove_agent (Request *req, NMSecretAgent *agent, GSList **pending_reqs)
 		_LOGD (agent, "current agent removed from secrets request "LOG_REQ_FMT,
 		       LOG_REQ_ARG (req));
 		*pending_reqs = g_slist_prepend (*pending_reqs, req);
-	} else {
+	} else if (g_slist_find (req->pending, agent)) {
 		req->pending = g_slist_remove (req->pending, agent);
 
 		_LOGD (agent, "agent removed from secrets request "LOG_REQ_FMT,