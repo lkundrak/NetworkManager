lkundrak/NetworkManager

commit 9a88d72fd4a289d924ce4a6703775b0d5c9868be

core: register G_TYPE_SOCKET early
Concurrent registrations later on can cause a deadlock due to a GLib bug.

https://bugzilla.gnome.org/show_bug.cgi?id=754795

src/main.c
@@ -257,6 +257,12 @@ main (int argc, char *argv[])
 
 	nm_g_type_init ();
 
+	/* Known to cause a possible deadlock unpon GDBus initialization:
+	 * https://bugzilla.gnome.org/show_bug.cgi?id=674885 */
+	g_type_ensure (G_TYPE_SOCKET);
+	g_type_ensure (G_TYPE_DBUS_CONNECTION);
+	g_type_ensure (NM_TYPE_BUS_MANAGER);
+
 	_nm_utils_is_manager_process = TRUE;
 
 	main_loop = g_main_loop_new (NULL, FALSE);

commit d10446e5ad897832ac45b738ab6a373898ecb6b0

trivial: fix spelling in comment
Fixes: 9a88d72fd4a289d924ce4a6703775b0d5c9868be

src/main.c
@@ -257,7 +257,7 @@ main (int argc, char *argv[])
 
 	nm_g_type_init ();
 
-	/* Known to cause a possible deadlock unpon GDBus initialization:
+	/* Known to cause a possible deadlock upon GDBus initialization:
 	 * https://bugzilla.gnome.org/show_bug.cgi?id=674885 */
 	g_type_ensure (G_TYPE_SOCKET);
 	g_type_ensure (G_TYPE_DBUS_CONNECTION);