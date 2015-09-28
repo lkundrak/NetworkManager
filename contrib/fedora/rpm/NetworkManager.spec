lkundrak/NetworkManager

commit 2318c0de9291b9dcf9e720a2e5041266449dfd57

contrib/rpm: fix creating proper po/Makefile.in.in (rh #1265117)
Without that DATADIRNAME was not present in po/Makefile.in.in
and it resulted in /usr/\@DATADIRNAME\@/locale/cs/LC_MESSAGES/ path instead of
/usr/share/locale/cs/LC_MESSAGES/.

https://bugzilla.redhat.com/show_bug.cgi?id=1265117

contrib/fedora/rpm/NetworkManager.spec
@@ -343,8 +343,8 @@ by nm-connection-editor and nm-applet in a non-graphical environment.
 cp -R docs ORIG-docs
 %endif
 
-#autopoint --force
-#intltoolize --force
+autopoint --force
+intltoolize --automake --copy --force
 %configure \
 	--disable-static \
 	--with-dhclient=yes \

commit 1d56c3999b1f9554e47434e4ba80c2bca850325d

contrib/rpm: regenerate automake with locally present version
The modification of po/Makefile.in.in may trigger re-run of autotools and it
would fail if the versions differ.

Fixes: 2318c0de9291b9dcf9e720a2e5041266449dfd57

contrib/fedora/rpm/NetworkManager.spec
@@ -116,7 +116,6 @@ BuildRequires: wireless-tools-devel >= %{wireless_tools_version}
 BuildRequires: glib2-devel >= %{glib2_version}
 BuildRequires: gobject-introspection-devel >= 0.10.3
 BuildRequires: gettext-devel
-BuildRequires: /usr/bin/autopoint
 BuildRequires: pkgconfig
 BuildRequires: libnl3-devel >= %{libnl3_version}
 BuildRequires: perl(XML::Parser)
@@ -343,7 +342,7 @@ by nm-connection-editor and nm-applet in a non-graphical environment.
 cp -R docs ORIG-docs
 %endif
 
-autopoint --force
+autoreconf --install --force
 intltoolize --automake --copy --force
 %configure \
 	--disable-static \