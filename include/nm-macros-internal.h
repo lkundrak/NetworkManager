lkundrak/NetworkManager

commit c33416178f88353b2f424a18c576de09b6ae3215

macros: allow NULL pointer in nm_clear_g_signal_handler()
It does not make sense to issue an error. This should be a helper function.
"NetworkManager[18341]: nm_clear_g_signal_handler: assertion 'G_IS_OBJECT (self)' failed"
error started since commit e6d7fee5a617632acae02e12b1ec6156842df788 due to that.

include/nm-macros-internal.h
@@ -210,7 +210,8 @@ nm_clear_g_source (guint *id)
 static inline gboolean
 nm_clear_g_signal_handler (gpointer self, guint *id)
 {
-	g_return_val_if_fail (G_IS_OBJECT (self), FALSE);
+	if (!self)
+		return FALSE;
 
 	if (id && *id) {
 		g_signal_handler_disconnect (self, *id);

commit 4bb1a22d13c719f4d44cd6b7003908f446d4107f

macros: remove early return from nm_clear_g_signal_handler()
It is valid to call nm_clear_g_signal_handler() with missing
@self argument if (and only if) the @id is unspecified as well.

Remove the check for @self to get an assertion in case @id
is missing *and* @self is invalid. In this case,
g_signal_handler_disconnect() will raise a g_critical() for us.

Fixes: c33416178f88353b2f424a18c576de09b6ae3215

include/nm-macros-internal.h
@@ -210,9 +210,6 @@ nm_clear_g_source (guint *id)
 static inline gboolean
 nm_clear_g_signal_handler (gpointer self, guint *id)
 {
-	if (!self)
-		return FALSE;
-
 	if (id && *id) {
 		g_signal_handler_disconnect (self, *id);
 		*id = 0;