lkundrak/NetworkManager

commit 5d1cac81a0bcc66b891bc38f1a2cc4b84d47f08b

agent-manager: cancel secrets requests on an error
It might be that the user didn't supply the secrets in time and the dbus call
timed out. The agent should now hide the secrets dialog and we must let it know.

https://bugzilla.redhat.com/show_bug.cgi?id=1272023

src/settings/nm-agent-manager.c
@@ -841,6 +841,9 @@ _con_get_request_done (NMSecretAgent *agent,
 			req_complete_error (req, error);
 			g_error_free (error);
 		} else {
+			/* Tell the failed agent we're no longer interested. */
+			nm_secret_agent_cancel_secrets (parent->current, parent->current_call_id);
+
 			/* Try the next agent */
 			request_next_agent (req);
 			maybe_remove_agent_on_error (agent, error);

commit b70c90abf964fbd91d3d53a818ef03984a6f4bbe

agent-manager: don't try to cancel requests that already finished
Fixes: 5d1cac81a0bcc66b891bc38f1a2cc4b84d47f08b

src/settings/nm-agent-manager.c
@@ -841,8 +841,10 @@ _con_get_request_done (NMSecretAgent *agent,
 			req_complete_error (req, error);
 			g_error_free (error);
 		} else {
-			/* Tell the failed agent we're no longer interested. */
-			nm_secret_agent_cancel_secrets (req->current, req->current_call_id);
+			if (req->current_call_id) {
+				/* Tell the failed agent we're no longer interested. */
+				nm_secret_agent_cancel_secrets (req->current, req->current_call_id);
+			}
 
 			/* Try the next agent */
 			request_next_agent (req);